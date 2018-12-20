dec5524cc693f51359f54ffacad10726a2f19b87
Richard Ramos <info@richardramos.me>
2018-12-20 17:40:27 -0400

    docs: Added natspec for standard token

--- contracts/test/standardtoken.sol
+++ contracts/test/standardtoken.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.5.0;
 
 import "../erc20token.sol";
 
+
+/**
+ * @notice Standard ERC20 token for tests
+ */
 contract StandardToken is ERC20Token {
 
     uint256 private supply;
@@ -10,6 +14,12 @@ contract StandardToken is ERC20Token {
 
     constructor() public { }
     
+    /**
+     * @notice send `_value` token to `_to` from `msg.sender`
+     * @param _to The address of the recipient
+     * @param _value The amount of token to be transferred
+     * @return Whether the transfer was successful or not
+     */
     function transfer(
         address _to,
         uint256 _value
@@ -20,6 +30,12 @@ contract StandardToken is ERC20Token {
         return transfer(msg.sender, _to, _value);    
     }
 
+    /**
+     * @notice `msg.sender` approves `_spender` to spend `_value` tokens
+     * @param _spender The address of the account able to transfer the tokens
+     * @param _value The amount of tokens to be approved for transfer
+     * @return Whether the approval was successful or not
+     */
     function approve(address _spender, uint256 _value) 
         external
         returns (bool success) 
@@ -29,6 +45,13 @@ contract StandardToken is ERC20Token {
         return true;
     }
 
+    /**
+     * @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value The amount of token to be transferred
+     * @return Whether the transfer was successful or not
+     */
     function transferFrom(
         address _from,
         address _to,
@@ -47,6 +70,10 @@ contract StandardToken is ERC20Token {
         }
     }
 
+    /**
+     * @param _owner The address from which the balance will be retrieved
+     * @return The balance
+     */
     function allowance(address _owner, address _spender) 
         external 
         view 
@@ -55,6 +82,11 @@ contract StandardToken is ERC20Token {
         return allowed[_owner][_spender];
     }
 
+    /**
+     * @param _owner The address of the account owning tokens
+     * @param _spender The address of the account able to transfer the tokens
+     * @return Amount of remaining tokens allowed to spent
+     */
     function balanceOf(address _owner) 
         external 
         view 
@@ -63,6 +95,9 @@ contract StandardToken is ERC20Token {
         return balances[_owner];
     }
     
+    /**
+     * @notice return total supply of tokens
+     */
     function totalSupply() 
         external 
         view 
@@ -71,6 +106,9 @@ contract StandardToken is ERC20Token {
         return supply;
     }
 
+    /**
+     * @notice Mints tokens for testing
+     */
     function mint(
         address _to,
         uint256 _amount
@@ -82,6 +120,13 @@ contract StandardToken is ERC20Token {
         emit Transfer(address(0), _to, _amount);
     }
 
+     /**
+     * @notice send `_value` token to `_to` from `_from`
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value The amount of token to be transferred
+     * @return Whether the transfer was successful or not
+     */
     function transfer(
         address _from, 
         address _to,
