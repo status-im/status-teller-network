a1d09d1fbc0fb2fc362125c24ee782049ebc2fca
Iuri Matias <iuri.matias@gmail.com>
2018-12-20 17:02:32 -0500

    remove unusued parameter from natspec

--- contracts/test/standardtoken.sol
+++ contracts/test/standardtoken.sol
@@ -13,7 +13,7 @@ contract StandardToken is ERC20Token {
     mapping (address => mapping (address => uint256)) allowed;
 
     constructor() public { }
-    
+
     /**
      * @notice send `_value` token to `_to` from `msg.sender`
      * @param _to The address of the recipient
@@ -23,11 +23,11 @@ contract StandardToken is ERC20Token {
     function transfer(
         address _to,
         uint256 _value
-    ) 
-        external 
+    )
+        external
         returns (bool success)
     {
-        return transfer(msg.sender, _to, _value);    
+        return transfer(msg.sender, _to, _value);
     }
 
     /**
@@ -36,9 +36,9 @@ contract StandardToken is ERC20Token {
      * @param _value The amount of tokens to be approved for transfer
      * @return Whether the approval was successful or not
      */
-    function approve(address _spender, uint256 _value) 
+    function approve(address _spender, uint256 _value)
         external
-        returns (bool success) 
+        returns (bool success)
     {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
@@ -61,22 +61,23 @@ contract StandardToken is ERC20Token {
         returns (bool success)
     {
         if (balances[_from] >= _value &&
-            allowed[_from][msg.sender] >= _value && 
+            allowed[_from][msg.sender] >= _value &&
             _value > 0) {
             allowed[_from][msg.sender] -= _value;
             return transfer(_from, _to, _value);
-        } else { 
-            return false; 
+        } else {
+            return false;
         }
     }
 
     /**
      * @param _owner The address from which the balance will be retrieved
+     * @param _spender The address of the account able to spend the tokens
      * @return The balance
      */
-    function allowance(address _owner, address _spender) 
-        external 
-        view 
+    function allowance(address _owner, address _spender)
+        external
+        view
         returns (uint256 remaining)
     {
         return allowed[_owner][_spender];
@@ -84,24 +85,23 @@ contract StandardToken is ERC20Token {
 
     /**
      * @param _owner The address of the account owning tokens
-     * @param _spender The address of the account able to transfer the tokens
      * @return Amount of remaining tokens allowed to spent
      */
-    function balanceOf(address _owner) 
-        external 
-        view 
-        returns (uint256 balance) 
+    function balanceOf(address _owner)
+        external
+        view
+        returns (uint256 balance)
     {
         return balances[_owner];
     }
-    
+
     /**
      * @notice return total supply of tokens
      */
-    function totalSupply() 
-        external 
-        view 
-        returns(uint256 currentTotalSupply) 
+    function totalSupply()
+        external
+        view
+        returns(uint256 currentTotalSupply)
     {
         return supply;
     }
@@ -112,7 +112,7 @@ contract StandardToken is ERC20Token {
     function mint(
         address _to,
         uint256 _amount
-    ) 
+    )
         public
     {
         balances[_to] += _amount;
@@ -128,11 +128,11 @@ contract StandardToken is ERC20Token {
      * @return Whether the transfer was successful or not
      */
     function transfer(
-        address _from, 
+        address _from,
         address _to,
         uint256 _value
     )
-        internal 
+        internal
         returns (bool success)
     {
         if (balances[_from] >= _value && _value > 0) {
@@ -140,8 +140,8 @@ contract StandardToken is ERC20Token {
             balances[_to] += _value;
             emit Transfer(_from, _to, _value);
             return true;
-        } else { 
-            return false; 
+        } else {
+            return false;
         }
     }
 
