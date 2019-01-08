21af1871d8026c2d15610288bcb580593baeb3f7
Iuri Matias <iuri.matias@gmail.com>
2019-01-08 17:42:54 -0500

    add storybook knobs to price component

--- stories/price.stories.jsx
+++ stories/price.stories.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { withInfo } from "@storybook/addon-info";
 import { action } from '@storybook/addon-actions';
 
 import Price from '../app/js/components/Price';
 
-storiesOf('Price', module)
-  .add(
+const stories = storiesOf('Price', module)
+
+stories.addDecorator(withKnobs)
+stories.add(
     "Display Price",
     withInfo({ inline: true })(() => (
-      <Price logo="https://raw.githubusercontent.com/TrustWallet/tokens/master/coins/60.png" priceTicker="ETH/USD" price="1.23" />
-    ))
-  );
+      <Price logo={text('logo', "https://raw.githubusercontent.com/TrustWallet/tokens/master/coins/60.png")} priceTicker={text('ticker name', "ETH/USD")} price={number('price', 1.23)} />
+    )),
+);
