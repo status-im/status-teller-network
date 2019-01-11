4c7f2c83b45d61155c8c5abcfc54e34919cacaff
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-01-11 15:05:20 -0500

    chore(storybook): add escrow list story

--- app/js/features/escrow/helpers.js
+++ app/js/features/escrow/helpers.js
@@ -12,7 +12,7 @@ export function getEscrowState(escrow) {
   if (escrow.canceled) {
     return escrowStates.canceled;
   }
-  if (escrow.expired <= Date.now() / 1000) {
+  if (escrow.expirationTime <= Date.now() / 1000) {
     return escrowStates.expired;
   }
   return escrowStates.waiting;
