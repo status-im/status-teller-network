eb2593f00f1e3aeb838116518949042578e4d978
Iuri Matias <iuri.matias@gmail.com>
2019-01-17 14:09:02 -0500

    display error when there is issues fetching the prices instead of crashing

--- app/js/containers/PriceContainer.jsx
+++ app/js/containers/PriceContainer.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getEthUsdPrice, getSntUsdPrice } from '../features/prices/reducer';
+import { getEthUsdPrice, getSntUsdPrice, hasPricesError } from '../features/prices/reducer';
 
 import Price from '../components/Price';
 
-const PriceContainer = ({ ethUsd, sntUsd }) => (
+const PriceContainer = ({ ethUsd, sntUsd, hasErrors }) => (
   <div>
-    <Price logo="https://raw.githubusercontent.com/TrustWallet/tokens/master/coins/60.png" priceTicker="ETH/USD" price={ethUsd} />
-    <Price logo="https://raw.githubusercontent.com/TrustWallet/tokens/master/images/0x744d70fdbe2ba4cf95131626614a1763df805b9e.png" priceTicker="SNT/USD" price={sntUsd} />
+    {hasErrors &&
+      <p>Could not fetch prices</p>
+    }
+    {!hasErrors &&
+      <div>
+        <Price logo="https://raw.githubusercontent.com/TrustWallet/tokens/master/coins/60.png" priceTicker="ETH/USD" price={ethUsd} />
+        <Price logo="https://raw.githubusercontent.com/TrustWallet/tokens/master/images/0x744d70fdbe2ba4cf95131626614a1763df805b9e.png" priceTicker="SNT/USD" price={sntUsd} />
+      </div>
+    }
   </div>
 );
 
 const mapStateToProps = state => ({
   ethUsd: getEthUsdPrice(state),
-  sntUsd: getSntUsdPrice(state)
+  sntUsd: getSntUsdPrice(state),
+  hasErrors: hasPricesError(state)
 });
 
 PriceContainer.propTypes = {
