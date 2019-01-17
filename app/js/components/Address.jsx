e342019fc6d49d93f20ba6d00a0936cf2c7cea97
Richard Ramos <info@richardramos.me>
2019-01-17 16:17:07 -0400

    feat(@storybook) add knobs to address module

--- app/js/components/Address.jsx
+++ app/js/components/Address.jsx
@@ -4,9 +4,13 @@ import {compactAddress} from "../utils.js";
 
 const Address = (props) => <span title={props.address}>{props.compact ? compactAddress(props.address) : props.address}</span>;
 
+Address.defaultProps = {
+  compact: false
+};
+
 Address.propTypes = {
   address: PropTypes.string,
-  compact: PropTypes.bool
+  compact: PropTypes.bool.n
 };
 
 export default Address;
--- stories/address.stories.jsx
+++ stories/address.stories.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 import {storiesOf} from '@storybook/react';
 import {withInfo} from "@storybook/addon-info";
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 import Address from '../app/js/components/Address';
 
 
 storiesOf('Address', module)
+  .addDecorator(withKnobs)
   .add(
     "Normal",
     withInfo({inline: true})(() => (
-      <Address address="0x1122334455667788990011223344556677889900" compact={false} />
+      <Address address={text('Address', "0x1122334455667788990011223344556677889900")} compact={boolean('Compact', false)} />
     ))
   )
   .add(
     "Compact",
     withInfo({inline: true})(() => (
-      <Address address="0x1122334455667788990011223344556677889900" compact={true} />
+      <Address address={text('Address', "0x1122334455667788990011223344556677889900")} compact={boolean('Compact', true)} />
     ))
   );
