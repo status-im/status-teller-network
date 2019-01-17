ce52a6c199c6b3d780fdb330a10f262c03229f2a
Iuri Matias <iuri.matias@gmail.com>
2019-01-17 13:23:35 -0500

    display rating only if license has been bought; display msg when no rating is available; add examples with knobs to storybook

--- stories/license.stories.jsx
+++ stories/license.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 import { withInfo } from "@storybook/addon-info";
 import { action } from '@storybook/addon-actions';
 
@@ -8,15 +9,27 @@ import License from '../app/js/components/License';
 
 const info = {inline: true, propTables: [License.WrappedComponent]};
 
-storiesOf('License', module)
+const stories = storiesOf('License', module)
+
+stories.addDecorator(withKnobs);
+
+stories
   .add(
     "Display License",
     withInfo(info)(() => (
       <License buyLicense={action("buy-license")}/>
     ))
-  ).add(
-    "Display License when already own one",
-    withInfo(info)(() => (
+  )
+  .add(
+    "Display License when already own one (no rating)",
+    withInfo({ inline: true })(() => (
       <License isLicenseOwner/>
     ))
+  )
+  .add(
+    "Display License when already own one",
+    withInfo({ inline: true })(() => (
+      <License isLicenseOwner userRating={number('rating', '3')} />
+    ))
   );
+
