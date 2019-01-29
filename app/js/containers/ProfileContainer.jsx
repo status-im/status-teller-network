0edf2f9e6fe1b2cf695d613b2318cbcfffeb9e06
Anthony Laibe <anthony@laibe.cc>
2019-01-29 10:06:05 +0000

    Add skeleton for profile

--- app/js/containers/ProfileContainer.jsx
+++ app/js/containers/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, NavLink, Button } from 'reactstrap';
+import { Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
 import { Link } from "react-router-dom";
 import {withNamespaces} from 'react-i18next';
 
@@ -9,7 +9,53 @@ class ProfileContainer extends Component {
     const t = this.props.t;
     return (
       <Fragment>
-        <p>Profile</p>
+        <Row>
+          Denis
+        </Row>
+        <Row className="mt-4">
+          <Col xs="12">
+            <h5>My Reputation</h5>
+          </Col>
+          <Col xs="12">
+            <Card body>
+              hello
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col xs="12">
+            <span className="h5">My Trades</span>
+            <Link to="find-offer" className="float-right">Find offer</Link>
+          </Col>
+          <Col xs="12">
+            <Card body>
+              hello
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col xs="12">
+            <span className="h5">My Offers</span>
+            <Link to="create-offer" className="float-right">Create offer</Link>
+          </Col>
+          <Col xs="12">
+            <Card>
+              <CardHeader> Change </CardHeader>
+              <CardBody>hello</CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col xs="12">
+            <span className="h5">Status Contact Code</span>
+            <Link to="edit-status-contract-code" className="float-right">Edit</Link>
+          </Col>
+          <Col xs="12">
+            <Card body>
+              hello
+            </Card>
+          </Col>
+        </Row>
       </Fragment>
     );
   }
