a2c25c0aa6349bc9b041ecb6b6702ef23027884b
Anthony Laibe <anthony@laibe.cc>
2019-02-04 09:42:16 +0100

    update home without h-100

--- app/js/containers/HomeContainer.jsx
+++ app/js/containers/HomeContainer.jsx
@@ -4,17 +4,19 @@ import { Row, Col, Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 import {withNamespaces} from 'react-i18next';
 
+import "./HomeContainer.scss";
+
 class HomeContainer extends Component {
   render() {
     const t = this.props.t;
     return (
-      <Fragment>
-        <Row className="h-75">
-          <Col xs={12} className="my-auto">
+      <div className="home">
+        <Row className="home-headline">
+          <Col xs={12}>
             <h1 className="text-center">{t('home.welcome')}</h1>
           </Col>
         </Row>
-        <Row className="mt-5">
+        <Row className="home--footer">
           <Col xs={6}>
             <Button tag={Link} color="info" block to="/buy">{t('home.buy')}</Button>
           </Col>
@@ -22,7 +24,7 @@ class HomeContainer extends Component {
             <Button tag={Link} color="info" block to="/sell">{t('home.sell')}</Button>
           </Col>
         </Row>
-      </Fragment>
+      </div>
     );
   }
 }
