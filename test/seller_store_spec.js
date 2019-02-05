5a1658a127f95abc170a4e6a4636e7e36b643ef6
Anthony Laibe <anthony@laibe.cc>
2019-02-05 14:21:44 +0000

    Separate offer and seller

--- test/seller_store_spec.js
+++ test/seller_store_spec.js
@@ -47,33 +47,46 @@ contract("SellerStore", function () {
     try {
       await SellerStore.methods.add(SNT.address, License.address, "London", "USD", "Iuri", [0], 0, 1).send();
     } catch(error) {
-      const size = await SellerStore.methods.size().call();
-      assert.strictEqual(size, '0');
+      const sellersSize = await SellerStore.methods.sellersSize().call();
+      assert.strictEqual(sellersSize, '0');
     }
   });
 
-  it("should allow to add new seller when license owner", async function () {
+  it("should allow to add new seller and offer when license owner", async function () {
     const encodedCall = License.methods.buy().encodeABI();
     await SNT.methods.approveAndCall(License.options.address, 10, encodedCall).send();
     await SellerStore.methods.add(SNT.address, License.address, "London", "USD", "Iuri", [0], 0, 1).send();
-    const size = await SellerStore.methods.size().call();
-    assert.strictEqual(size, '1');
+    const sellersSize = await SellerStore.methods.sellersSize().call();
+    assert.strictEqual(sellersSize, '1');
+    const offersSize = await SellerStore.methods.offersSize().call();
+    assert.strictEqual(offersSize, '1');
   });
 
-  it("should not allow to add new seller when margin is more than 100", async function () {
+  it("should allow to add new offer only when already a seller", async function () {
+    await SellerStore.methods.add(SNT.address, License.address, "London", "EUR", "Iuri", [0], 0, 1).send();
+    const sellersSize = await SellerStore.methods.sellersSize().call();
+    assert.strictEqual(sellersSize, '1');
+    const offersSize = await SellerStore.methods.offersSize().call();
+    assert.strictEqual(offersSize, '2');
+  });
+
+  it("should not allow to add new offer when margin is more than 100", async function () {
     try {
       await SellerStore.methods.add(SNT.address, License.address, "London", "USD", "Iuri", [0], 0, 101).send();
     } catch(error) {
-      const size = await SellerStore.methods.size().call();
-      assert.strictEqual(size, '1');
+      const sellersSize = await SellerStore.methods.sellersSize().call();
+      assert.strictEqual(sellersSize, '1');
     }
   });
 
-  it("should allow to update a seller", async function () {
-    await SellerStore.methods.update(0, SNT.address, License.address, "Paris", "USD", "Iuri", [0], 0, 1).send();
-    const size = await SellerStore.methods.size().call();
-    assert.strictEqual(size, '1');
+  it("should allow to update a seller and offer", async function () {
+    await SellerStore.methods.update(0, SNT.address, License.address, "Paris", "EUR", "Iuri", [0], 0, 1).send();
+    const sellersSize = await SellerStore.methods.sellersSize().call();
+    assert.strictEqual(sellersSize, '1');
     const seller = await SellerStore.methods.sellers(0).call();
     assert.strictEqual(seller.location, 'Paris');
+
+    const offer = await SellerStore.methods.offers(0).call();
+    assert.strictEqual(offer.currency, 'EUR');
   });
 });
