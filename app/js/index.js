68a0cf2622b585264214f2bbcb4f5a5aa099c454
Anthony Laibe <anthony@laibe.cc>
2019-02-14 13:20:41 +0000

    Use redux-persist for local state

--- app/js/index.js
+++ app/js/index.js
@@ -4,6 +4,7 @@ import { ConnectedRouter } from "connected-react-router";
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import '../css/fonts/Inter/inter.css';
 import '../css/bootstrap-overrides.scss';
@@ -12,15 +13,17 @@ import '../css/Form.scss';
 
 import App from './layout/App';
 import history from './history';
-import store from './store';
+import {store, persistor} from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <I18nextProvider i18n={ i18n }>
-        <App />
-      </I18nextProvider>
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <I18nextProvider i18n={ i18n }>
+          <App />
+        </I18nextProvider>
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- app/js/store.js
+++ app/js/store.js
@@ -2,6 +2,8 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 import { all } from 'redux-saga/effects';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import history from './history';
 import rootReducer from './reducer';
@@ -14,6 +16,12 @@ import signature from './features/signature';
 import arbitration from './features/arbitration';
 import metadata from './features/metadata';
 import balances from './features/balances';
+
+const persistConfig = {
+  key: 'teller-network-store',
+  storage,
+  blacklist: ['embarkjs']
+};
 
 function *root() {
   yield all([
@@ -33,7 +41,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  connectRouter(history)(rootReducer),
+  connectRouter(history)(persistReducer(persistConfig, rootReducer)),
   composeEnhancers(
     applyMiddleware(
       routerMiddleware(history),
@@ -43,5 +51,6 @@ const store = createStore(
 );
 
 sagaMiddleware.run(root);
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
