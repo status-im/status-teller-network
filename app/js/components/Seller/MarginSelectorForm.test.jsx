de36227f5a3755659ca72629ddba2f918f3c8149
Anthony Laibe <anthony@laibe.cc>
2019-02-20 11:16:07 +0000

    Search of asset

--- app/js/components/Seller/MarginSelectorForm.test.jsx
+++ app/js/components/Seller/MarginSelectorForm.test.jsx
@@ -6,6 +6,8 @@ import MarginSelectorForm from "./MarginSelectorForm";
 describe('MarginSelectorForm', () => {
   it('should render correctly', () => {
     const component = shallow(<MarginSelectorForm margin={0}
+                                                  token={{symbol: 'SNT'}}
+                                                  prices={{'SNT': {'USD': 1}}}
                                                   marketType={0}
                                                   currency={""}
                                                   onMarketTypeChange={() => undefined}
