97b2e0753a1edb5f3903e73e36ad54ce3ba0328d
Anthony Laibe <anthony@laibe.cc>
2019-02-21 12:22:06 +0000

    Seller profile container load data

--- app/js/containers/Buyer/SellerProfileContainer.jsx
+++ app/js/containers/Buyer/SellerProfileContainer.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {withRouter} from "react-router-dom";
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+
 import SellerInformation from '../../components/SellerInformation';
 import SellerOfferList from '../../components/Buyer/SellerOfferList';
 import Map from '../../components/Buyer/Map';
-import PropTypes from 'prop-types';
-import {connect} from "react-redux";
 import buyer from "../../features/buyer";
-import {withRouter} from "react-router-dom";
+import metadata from "../../features/metadata";
+
 
 import ETH from "../../../images/tokens/ETH.png";
 import SNT from "../../../images/tokens/SNT.png";
@@ -19,11 +22,9 @@ const FAKE_OFFERS = [
   {asset: {name: 'ZRX', icon: ZRX}, min: 200, max: 600, fiat: '$'}
 ];
 
-class ProfileContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.address = this.props.match.params.address;
-    // TODO get seller information
+class SellerProfileContainer extends Component {
+  componentDidMount() {
+    this.props.load(this.props.match.params.address);
   }
 
   offerClick = (offerId) => {
@@ -32,27 +33,38 @@ class ProfileContainer extends Component {
   };
 
   render() {
+    const profile = this.props.profile;
     return (
       <div className="seller-profile-container">
-        <SellerInformation name="Roger" reputation={{upCount: 442, downCount: 32}} address={this.address} />
-        <h3 className="mt-3">Location</h3>
+        <SellerInformation username={profile.username} reputation={profile.reputation} address={profile.address} />
+        <h3 className="mt-3">{profile.location}</h3>
         <Map coords={{latitude: 45.492611, longitude: -73.617959}} markerOnly={true}/>
         <p className="text-muted mt-2">Saalestraße 39A, 12055 Berlin</p>
-        <SellerOfferList offers={FAKE_OFFERS} onClick={this.offerClick}/>
+        <SellerOfferList offers={profile.offers} onClick={this.offerClick}/>
       </div>
     );
   }
 }
 
-ProfileContainer.propTypes = {
+SellerProfileContainer.propTypes = {
   match: PropTypes.object,
   setOffer: PropTypes.func,
-  history: PropTypes.object
+  load: PropTypes.func,
+  history: PropTypes.object,
+  profile: PropTypes.object
+};
+
+const mapStateToProps = (state, props) => {
+  const address = props.match.params.address;
+  return {
+    profile: metadata.selectors.getProfile(state, address)
+  };
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   {
+    load: metadata.actions.load,
     setOffer: buyer.actions.setOffer
   }
-)(withRouter(ProfileContainer));
+)(withRouter(SellerProfileContainer));
