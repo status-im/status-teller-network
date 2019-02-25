616a3e6be737a87b39530c6110043e10163c22b4
Anthony Laibe <anthony@laibe.cc>
2019-02-25 15:00:22 +0000

    feat: hide profile on profile page

--- app/js/layout/Header/index.jsx
+++ app/js/layout/Header/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import {withRouter, Link} from "react-router-dom";
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBullseye} from "@fortawesome/free-solid-svg-icons";
 import {Navbar, NavbarBrand, Nav, NavLink, NavItem} from 'reactstrap';
 
 import "./index.scss";
 
-const Header = ({profile}) => (
+const Header = ({profile, history}) => (
   <header>
     <Navbar expand="md" className="px-0">
       <NavbarBrand tag={Link} to="/"><FontAwesomeIcon icon={faBullseye} className="mr-2"/><span className="text-body text-logo">TN</span></NavbarBrand>
       <Nav className="ml-auto" navbar>
         <NavItem>
-          {profile && <NavLink tag={Link} to="/profile/">Profile</NavLink>}
+          {profile && history.location.pathname !== '/profile' && <NavLink tag={Link} to="/profile">Profile</NavLink>}
         </NavItem>
       </Nav>
     </Navbar>
@@ -21,7 +21,8 @@ const Header = ({profile}) => (
 );
 
 Header.propTypes = {
+  history: PropTypes.object,
   profile: PropTypes.object
 };
 
-export default Header;
+export default withRouter(Header);
