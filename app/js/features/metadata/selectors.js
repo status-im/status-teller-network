754de28c11e60de067cb680a1c529241c1169952
Richard Ramos <info@richardramos.me>
2019-02-25 09:25:41 -0400

    : display full address or compact on click / press and change datasource text

--- app/js/features/metadata/selectors.js
+++ app/js/features/metadata/selectors.js
@@ -6,7 +6,7 @@ function enhanceOffer(state, offer) {
   return {
     ...offer,
     paymentMethodsForHuman: offer.paymentMethods.map((i) => PAYMENT_METHODS[i]).join(', '),
-    rateForHuman: `${offer.margin}% ${MARKET_TYPES[offer.marketType]} Bitfinex`,
+    rateForHuman: `${offer.margin}% ${MARKET_TYPES[offer.marketType]} CryptoCompare`,
     token: Object.values(state.network.tokens).find((token) => token.address === offer.asset)
   };
 }
