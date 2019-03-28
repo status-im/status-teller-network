00711e477d304dc439bb472696c08ac5e8756be2
Richard Ramos <info@richardramos.me>
2019-03-28 18:11:32 -0400

    add reducers to blacklist

--- app/js/store.js
+++ app/js/store.js
@@ -22,7 +22,7 @@ const persistConfig = {
   key: 'teller-network-store',
   storage,
   stateReconciler: autoMergeLevel2,
-  blacklist: ["network"]
+  blacklist: ['network', 'escrow', 'approval']
 };
 
 function *root() {
