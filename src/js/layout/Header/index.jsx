0ef4effc630f1db620367557fa75acb7cc5e054e
Richard Ramos <info@richardramos.me>
2019-04-03 13:20:18 -0400

    feat: set tmp logo (#158)
    * feat: set tmp logo
    * refactor: code review

--- src/js/layout/Header/index.jsx
+++ src/js/layout/Header/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {withRouter, Link} from "react-router-dom";
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBullseye} from "@fortawesome/free-solid-svg-icons";
 import {Navbar, NavbarBrand, Nav, NavLink, NavItem} from 'reactstrap';
 
 import "./index.scss";
+import logo from "../../../images/logo-small.svg";
+
 
 const Header = ({profile, history}) => (
   <header>
     <Navbar expand="md" className="px-0">
-      <NavbarBrand tag={Link} to="/"><FontAwesomeIcon icon={faBullseye} className="mr-2"/><span className="text-body text-logo">TN</span></NavbarBrand>
+      <NavbarBrand tag={Link} to="/"><img src={logo} alt="Logo" width="32" height="32" /><span className="text-body text-logo">TN</span></NavbarBrand>
       <Nav className="ml-auto" navbar>
         <NavItem>
           {profile && history.location.pathname !== '/profile' && <NavLink tag={Link} to="/profile">Profile</NavLink>}
