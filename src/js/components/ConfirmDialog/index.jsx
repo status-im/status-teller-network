32e8f05ac832ac43f81ff1705416403450116850
Richard Ramos <info@richardramos.me>
2019-04-04 09:57:40 -0400

    feat: confirm dialog (#159)
    * feat: confirm dialog
    * fix: code review
    * refactor: use onConfirm directly

---
+++ src/js/components/ConfirmDialog/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const ConfirmDialog = ({display, onCancel, title, content, onConfirm}) => (
+  <Modal isOpen={display} toggle={onCancel} backdrop={true} >
+    <ModalHeader toggle={onCancel}>
+      {title}
+    </ModalHeader>
+    <ModalBody>
+      <p>{content}</p>
+    </ModalBody>
+    <ModalFooter>
+      <Button onClick={onCancel}>Cancel</Button>
+      <Button onClick={onConfirm}>Yes</Button>
+    </ModalFooter>
+  </Modal>
+);
+
+
+ConfirmDialog.defaultProps = {
+  display: false
+};
+
+
+ConfirmDialog.propTypes = {
+  onCancel: PropTypes.func,
+  onConfirm: PropTypes.func,
+  title: PropTypes.string,
+  content: PropTypes.string,
+  display: PropTypes.bool
+};
+
+export default ConfirmDialog;
--- src/js/pages/Escrow/components/CancelEscrow.jsx
+++ src/js/pages/Escrow/components/CancelEscrow.jsx
@@ -1,26 +1,50 @@
 /* eslint-disable no-alert,no-restricted-globals */
-import React from 'react';
+import React, {Fragment, Component} from 'react';
 import {Row, Col} from 'reactstrap';
 import PropTypes from 'prop-types';
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 import RoundedIcon from "../../../ui/RoundedIcon";
 import escrow from '../../../features/escrow';
+import ConfirmDialog from "../../../components/ConfirmDialog";
 
 
-const CancelEscrow = ({cancelEscrow, trade}) => {
-  const shouldDisplay = trade.status === escrow.helpers.tradeStates.waiting || trade.status === escrow.helpers.tradeStates.funded;
-  return shouldDisplay && <a onClick={() => { if(confirm('Sure?')) cancelEscrow(trade.escrowId); }}>
-    <Row className="mt-4 text-primary">
-      <Col xs="2">
-        <RoundedIcon icon={faTimes} bgColor="blue"/>
-      </Col>
-      <Col xs="10" className="my-auto">
-        <h6 className="m-0">Cancel Trade</h6>
-      </Col>
-    </Row>
-  </a>;
-};
+class CancelEscrow extends Component {
+
+  state = {
+    displayDialog: false
+  }
+
+  displayDialog = show => (e) => {
+    if(e) e.preventDefault();
+    this.setState({displayDialog: show});
+    return false;
+  }
+
+  cancelEscrow = () => {
+    this.props.cancelEscrow(this.props.trade.escrowId);
+    this.displayDialog(false)();
+  }
+
+  render(){
+    const {trade} = this.props;
+    const shouldDisplay = trade.status === escrow.helpers.tradeStates.waiting || trade.status === escrow.helpers.tradeStates.funded;
+    return shouldDisplay && <Fragment>
+      <a href="#" onClick={this.displayDialog(true)}>
+        <Row className="mt-4 text-primary">
+          <Col xs="2">
+            <RoundedIcon icon={faTimes} bgColor="blue"/>
+          </Col>
+          <Col xs="10" className="my-auto">
+            <h6 className="m-0">Cancel Trade</h6>
+          </Col>
+        </Row>
+        
+      </a>
+      <ConfirmDialog display={this.state.displayDialog} onConfirm={this.cancelEscrow} onCancel={this.displayDialog(false)} title="Cancel Escrow" content="Are you sure?"   />
+    </Fragment>;
+  }
+}
 
 CancelEscrow.propTypes = {
   cancelEscrow: PropTypes.func,
