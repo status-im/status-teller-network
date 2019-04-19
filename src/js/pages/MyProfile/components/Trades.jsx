280c2435780fbd47c2e2ce63e43ede47f025c7ff
Richard Ramos <info@richardramos.me>
2019-04-19 14:45:40 -0400

    fix: set trade status colors (#180)

--- src/js/pages/MyProfile/components/Trades.jsx
+++ src/js/pages/MyProfile/components/Trades.jsx
@@ -4,9 +4,31 @@ import {Card, Row, Col} from 'reactstrap';
 import {Link} from "react-router-dom";
 import {withNamespaces} from 'react-i18next';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircle, faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import Identicon from "../../../components/UserInformation/Identicon";
 import {formatBalance} from "../../../utils/numbers";
+import {tradeStates} from "../../../features/escrow/helpers";
+
+const getTradeStyle = (tradeState) => {
+  switch(tradeState){
+    case tradeStates.waiting:
+    case tradeStates.funded:
+    case tradeStates.paid:
+      return {text: 'Open', className: 'bg-success'};
+    case tradeStates.released:
+      return {text: 'Closed', className: 'bg-primary'};
+    case tradeStates.canceled:
+      return {text: 'Canceled', className: 'bg-secondary'};
+    case tradeStates.expired:
+      return {text: 'Expired', className: 'bg-secondary'};
+    case tradeStates.arbitration_open:
+      return {text: 'Arbitration', className: 'bg-danger'};
+    case tradeStates.arbitration_closed:
+      return {text: 'Resolved', className: 'bg-primary'};
+    default:
+      return {text: tradeState, className: 'bg-secondary'};
+  }
+};
 
 class Trades extends Component {
   renderTrades() {
@@ -15,6 +37,7 @@ class Trades extends Component {
       <Card body className="py-2 px-3 shadow-sm">
         {this.props.trades.map((trade, index) => {
           const isBuyer = trade.buyer ===  address;
+          const tradeStyle = getTradeStyle(trade.status);
           return <Link key={index} to={"/escrow/" + trade.escrowId}>
             <Row className="my-1 border-bottom">
               <Col className="align-self-center pr-0" xs="2">
@@ -23,12 +46,11 @@ class Trades extends Component {
               <Col className="align-self-center" xs="3">
                 <span>{isBuyer ? trade.seller.username : trade.buyerInfo.username}</span>
               </Col>
-              <Col className="align-self-center" xs="4">
+              <Col className="align-self-center" xs="3">
                 {isBuyer ? 'Buy' : 'Sell' } {formatBalance(trade.tokenAmount)} {trade.token.symbol}
               </Col>
-              <Col className="align-self-center text-right text-success" xs="3">
-                <FontAwesomeIcon icon={faCircle} className="mr-2"/>
-                {trade.status}
+              <Col className="align-self-center text-center text-success" xs="4">
+                <span className={"p-1 text-uppercase d-inline text-white rounded-sm " + tradeStyle.className}>{tradeStyle.text}</span>
               </Col>
             </Row>
           </Link>;
