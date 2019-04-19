5969601feb81f4bc7eccb37d7268990fb275ee93
Richard Ramos <info@richardramos.me>
2019-04-19 15:00:32 -0400

    fix: match contact details page design to figma

--- src/js/components/EditContact/index.jsx
+++ src/js/components/EditContact/index.jsx
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react';
 import {FormGroup, Label, Button} from 'reactstrap';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
+import Textarea from 'react-validation/build/textarea';
 import {withNamespaces} from "react-i18next";
 import PropTypes from 'prop-types';
 import {required, isContactCode} from "../../validators";
@@ -9,28 +10,26 @@ import {required, isContactCode} from "../../validators";
 class EditContact extends Component {
   render() {
     const {t, username, statusContactCode, isStatus} = this.props;
-
     return (
       <Fragment>
         <h2>{t('contactForm.yourName')}</h2>
-        <p>{t('contactForm.bestWay')}</p>
-
         <Form>
           <FormGroup>
-            <Label for="nickname" className="text-dark">Set nickname</Label>
             <Input type="text"
                    name="nickname"
                    id="nickname"
+                   placeholder="Set nickname"
                    value={username}
                    className="form-control"
                    onChange={(e) => this.props.changeUsername(e.target.value)}
                    validations={[required]}/>
           </FormGroup>
           <FormGroup>
-            <Label for="contactCode" className="text-dark">Status contact code or Status ENS name</Label>
-            <Input type="text"
+            <Textarea type="text"
                    name="contactCode"
                    id="contactCode"
+                   rows="5"
+                   placeholder="Status contact code or Status ENS name"
                    value={statusContactCode}
                    className="form-control"
                    onChange={(e) => this.props.changeStatusContactCode(e.target.value)}
