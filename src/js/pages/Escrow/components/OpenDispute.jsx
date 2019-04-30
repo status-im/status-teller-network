eb0c90d64bfaa392b228ddafb65700bff75ab588
Richard Ramos <info@richardramos.me>
2019-04-30 17:19:40 -0400

    feat: open dispute screen

--- src/js/pages/Escrow/components/OpenDispute.jsx
+++ src/js/pages/Escrow/components/OpenDispute.jsx
@@ -4,17 +4,20 @@ import PropTypes from 'prop-types';
 import exclamationCircle from "../../../../images/exclamation-circle.png";
 import RoundedIcon from "../../../ui/RoundedIcon";
 import escrow from '../../../features/escrow';
+import {Link} from "react-router-dom";
 
 const OpenDispute = ({trade}) => {
   const shouldDisplay = trade.status !== escrow.helpers.tradeStates.waiting && trade.status !== escrow.helpers.tradeStates.funded;
-  return shouldDisplay && <Row className="mt-4 text-danger">
-    <Col xs="2">
-      <RoundedIcon image={exclamationCircle} bgColor="red"/>
-    </Col>
-    <Col xs="10" className="my-auto">
-      <h6 className="m-0 font-weight-normal">Open dispute</h6>
-    </Col>
-  </Row>;
+  return shouldDisplay && (
+    <Row className="mt-4 text-danger" tag={Link} to={"/openCase/" + trade.escrowId}>
+      <Col xs="2">
+        <RoundedIcon image={exclamationCircle} bgColor="red"/>
+      </Col>
+      <Col xs="10" className="my-auto">
+        <h6 className="m-0 font-weight-normal">Open dispute</h6>
+      </Col>
+    </Row>
+  );
 };
 
 OpenDispute.propTypes = {
