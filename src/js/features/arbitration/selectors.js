29d0888ab5d1ab5cece650edfd78e2a8e34a27a0
Richard Ramos <info@richardramos.me>
2019-05-03 20:28:13 -0400

    fix: tests

--- src/js/features/arbitration/selectors.js
+++ src/js/features/arbitration/selectors.js
@@ -21,5 +21,5 @@ export const getArbitration = (state) => {
 
 export const isLicenseOwner = state => state.arbitration.licenseOwner;
 export const isLoading = state => state.arbitration.loading;
-export const isError = state => { console.log(state.arbitration); return !!state.arbitration.error; }
+export const isError = state => !!state.arbitration.error;
 export const getLicensePrice = state => parseInt(state.arbitration.price, 10);
