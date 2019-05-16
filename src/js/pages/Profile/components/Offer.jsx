cb224911f48fb2f622e973dbbc94c7b265bcf894
Iuri Matias <iuri.matias@gmail.com>
2019-05-16 09:00:04 -0400

    fix generic logo in profile page

--- src/js/pages/Profile/components/Offer.jsx
+++ src/js/pages/Profile/components/Offer.jsx
@@ -9,7 +9,7 @@ import {calculateEscrowPrice} from '../../../utils/transaction';
 const Offer = ({offer, prices, onClick}) => (
   <Row className="border py-2 mx-0 my-2 rounded">
     <Col xs="4" className="v-align-center">
-      <p className="font-weight-bold"><img src={TokenImages[`${offer.token.symbol}.png`]} alt="asset icon" className="mr-2"/>{offer.token.symbol}</p>
+      <p className="font-weight-bold"><img src={TokenImages[`${offer.token.symbol}.png`] || TokenImages[`generic.png`]} alt="asset icon" className="mr-2"/>{offer.token.symbol}</p>
     </Col>
     <Col xs="8" className="v-align-center text-right">
       <Button color="primary p-2" onClick={onClick}>Buy for {truncateTwo(calculateEscrowPrice(offer, prices))} {offer.currency}</Button>
