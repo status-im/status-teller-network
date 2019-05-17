70545cd109f496510dd9431a5466dc265b544c0e
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-05-17 15:16:34 -0400

    feat: put seller balance as max value to trade

--- src/js/features/network/selectors.js
+++ src/js/features/network/selectors.js
@@ -5,6 +5,15 @@ export const getError = state => state.network.error;
 export const getAddress = state => state.network.address;
 export const isStatus = state => state.network.isStatus;
 export const getTokens = state => state.network.tokens;
+export const getBalance = (state, symbol, address) => {
+  if (!symbol) {
+    return null;
+  }
+  if (!address || address === state.network.address) {
+    return parseInt(state.network.tokens[symbol].balance, 10);
+  }
+  return state.network.tokens[symbol].balances ? parseInt(state.network.tokens[symbol].balances[address], 10) : null;
+};
 export const getTokensWithPositiveBalance = (state) => (
   Object.values(state.network.tokens).filter((token) => token.balance > 0)
 );
