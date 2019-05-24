c26b89f280993c873c96b011849264c8f8e28cc4
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-05-24 10:27:29 -0400

    fix: arbitration styling

--- src/js/pages/Arbitration/components/EscrowDetail.jsx
+++ src/js/pages/Arbitration/components/EscrowDetail.jsx
@@ -5,9 +5,9 @@ import PropTypes from 'prop-types';
 const EscrowDetail = ({escrow}) => <Row className="mt-4">
     <Col xs="10">
       <h5 className="m-0">Trade details</h5>
-      <p className="text-dark m-0">{(escrow.tokenAmount * escrow.assetPrice / 100).toFixed(2)} {escrow.offer.currency} for {escrow.tokenAmount} {escrow.token.symbol}</p>
-      <p className="text-dark m-0">{escrow.token.symbol} Price = {escrow.assetPrice / 100} {escrow.offer.currency}</p>
-      <p className="text-dark m-0">Took place on {escrow.createDate}</p>
+      <p className="m-0">{(escrow.tokenAmount * escrow.assetPrice / 100).toFixed(2)} {escrow.offer.currency} for {escrow.tokenAmount} {escrow.token.symbol}</p>
+      <p className="m-0">{escrow.token.symbol} Price = {escrow.assetPrice / 100} {escrow.offer.currency}</p>
+      <p className="m-0">Took place on {escrow.createDate}</p>
     </Col>
   </Row>;
 
