c140e6018fec2e7ef903bb1bf6ad75406759a0e7
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-05-29 15:11:33 -0400

    feat: improve trade page to make it clearer

--- src/js/wizards/Buy/1_Trade/components/OfferTrade.jsx
+++ src/js/wizards/Buy/1_Trade/components/OfferTrade.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {withNamespaces} from "react-i18next";
-import {Row, Col, FormGroup, Button, UncontrolledTooltip} from 'reactstrap';
+import {Row, Col, FormGroup, Button, UncontrolledTooltip, Label} from 'reactstrap';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import {isNumber, lowerEqThan, higherEqThan} from "../../../../validators";
@@ -25,16 +25,30 @@ const OfferTrade = ({
     <Col xs="12" className="mt-4">
       <Form className="text-center">
         <FormGroup>
-          <Input type="number" name="fiat" className="form-control" value={currencyQuantity}
-                 validations={[isNumber]}
-                 placeholder="Fiat quantity" onChange={(e) => onCurrencyChange(e.target.value)} step="any" />
-          <span className="input-icon">{currency.id}</span>
+          <Row>
+            <Col xs={2} sm={1} className="v-align-center">
+              <Label for="asset-quantity-input">Buy:</Label>
+            </Col>
+            <Col xs={10} sm={11}>
+              <Input type="text" name="asset" className="form-control" value={assetQuantity} id="asset-quantity-input"
+                     validations={[isNumber, lowerEqThan.bind(null, parseFloat(maxToken)), higherEqThan.bind(null, parseFloat(minToken))]}
+                     placeholder="Asset quantity" onChange={(e) => onAssetChange(e.target.value)} step="any"/>
+              <span className="input-icon mr-3">{asset}</span>
+            </Col>
+          </Row>
         </FormGroup>
         <FormGroup>
-          <Input type="number" name="asset" className="form-control" value={assetQuantity}
-                 validations={[isNumber, lowerEqThan.bind(null, parseFloat(maxToken)),  higherEqThan.bind(null, parseFloat(minToken))]}
-                 placeholder="Asset quantity" onChange={(e) => onAssetChange(e.target.value)} step="any" />
-          <span className="input-icon">{asset}</span>
+          <Row>
+            <Col xs={2} sm={1} className="v-align-center">
+              <Label for="fiat-quantity-input" className="align-baseline">For:</Label>
+            </Col>
+            <Col xs={10} sm={11}>
+              <Input type="text" name="fiat" className="form-control" value={currencyQuantity}
+                     validations={[isNumber]} id="fiat-quantity-input"
+                     placeholder="Fiat quantity" onChange={(e) => onCurrencyChange(e.target.value)} step="any"/>
+              <span className="input-icon mr-3">{currency.id}</span>
+            </Col>
+          </Row>
         </FormGroup>
         {disabled && <p className="text-muted">{t('buyer.offerTrade.enterBefore')}</p>}
         <Button color="primary" className="px-4" onClick={onClick} disabled={disabled}>Open trade</Button>
