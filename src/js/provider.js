b60bc3f0e84a2cdf5f9539e70dfdcca532e46172
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-06-11 08:42:42 -0400

    Fix using the custom provider in local (#308)
    * fix: fix local provider set by just extending the provider
    * Adapting sendAsync

--- src/js/provider.js
+++ src/js/provider.js
@@ -1,4 +1,3 @@
-/* global web3 */
 import Escrow from '../embarkArtifacts/contracts/Escrow';
 import {checkNotEnoughETH} from './utils/transaction';
 import {addressCompare} from './utils/address';
@@ -11,19 +10,21 @@ const VALID_OPERATIONS = {
 };
 
 class Provider {
-    constructor(origProvider, relayProvider) {
-        this.origProvider = origProvider;
-        this.relayProvider = relayProvider;
-        this.origProviderSend = (this.origProvider['sendAsync'] || this.origProvider['send']).bind(this.origProvider);
-        this.relayProviderSend = (this.relayProvider['sendAsync'] || this.relayProvider['send']).bind(this.relayProvider);
-    }
+  constructor(relayProvider) {
+    this.relayProvider = relayProvider;
+    this.relayProviderSend = (this.relayProvider['sendAsync'] || this.relayProvider['send']).bind(this.relayProvider);
+  }
 
-    send(payload, callback) {
+  startProvider(web3) {
+    this.origProvider = web3.currentProvider;
+    this.origProviderSend = (this.origProvider['sendAsync'] || this.origProvider['send']).bind(this.origProvider);
+    
+    const fSend = (payload, callback) => {
       const params = payload.params[0];
 
-      if(!(params && params.to && addressCompare(params.to, Escrow.options.address.toLowerCase()) && 
-           payload.method === "eth_sendTransaction" && 
-           Object.values(VALID_OPERATIONS).includes(params.data.substring(2, 10)))){
+      if (!(params && params.to && addressCompare(params.to, Escrow.options.address.toLowerCase()) &&
+        payload.method === "eth_sendTransaction" &&
+        Object.values(VALID_OPERATIONS).includes(params.data.substring(2, 10)))) {
         this.origProviderSend(payload, callback);
         return;
       }
@@ -31,26 +32,30 @@ class Provider {
       (async () => {
         const balance = await web3.eth.getBalance(web3.eth.defaultAccount);
         const gasPrice = await web3.eth.getGasPrice();
-        if(checkNotEnoughETH(gasPrice, balance)){
-          // Increase 120%. 
+        if (checkNotEnoughETH(gasPrice, balance)) {
+          // Increase 120%.
           // Normally we would use gaspriceFactorPercent on tabookey.RelayProvider
           // but this version of web3 does a getPrice before send() if the gas price is null
           // to set this gas price as a parameter. Tabookey will then use this value directly
           // without applying the factor percent
           const useGasPrice = web3.utils.toBN(gasPrice).mul(web3.utils.toBN("120")).div(web3.utils.toBN("100"));
           payload.params[0].gasPrice = web3.utils.toHex(useGasPrice);
-          this.relayProviderSend(payload, function (error, result) {
+          this.relayProviderSend(payload, (error, result) => {
             callback(error, result);
           });
         } else {
           this.origProviderSend(payload, callback);
         }
       })();
-    }
+    };
+    
+    this.origProvider.send = fSend;
+    this.origProvider.sendAsync = fSend;
+  }
 
-    sendAsync(payload, callback) {
-        return this.send(payload, callback);
-    }
+  sendAsync(payload, callback) {
+    return this.send(payload, callback);
+  }
 }
 
 export default Provider;
--- src/js/services/embarkjs.js
+++ src/js/services/embarkjs.js
@@ -11,11 +11,11 @@ export function onReady() {
         return reject(err);
       }
 
-      // A relay gets compensated whenever it relays a transaction: whatever the gas usage it pays, 
+      // A relay gets compensated whenever it relays a transaction: whatever the gas usage it pays,
       // it gets back the same plus the "txFee" precent - that is, it gets back ( (txFee+100)*gasUsed ) / 100
       const relayProvider = new tabookey.RelayProvider(web3.currentProvider, { txfee: 12 });
-      const customProvider = new TellerProvider(web3.currentProvider, relayProvider);
-      web3.setProvider(customProvider);
+      const customProvider = new TellerProvider(relayProvider);
+      customProvider.startProvider(web3);
 
       resolve();
     });
