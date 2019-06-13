ccc14b5d45eb4bfb9f0148bfb98302634645cfb2
Richard Ramos <info@richardramos.me>
2019-06-13 13:47:15 -0400

    feat: adds fiat currencies handled by status app

--- src/js/features/prices/saga.js
+++ src/js/features/prices/saga.js
@@ -2,7 +2,8 @@ import { fork, take, takeEvery, call, put, actionChannel, select } from 'redux-s
 import cc from 'cryptocompare';
 import { FETCH_PRICES, FETCH_PRICES_SUCCEEDED, FETCH_PRICES_FAILED, FETCH_EXCHANGE_RATE } from './constants';
 import network from "../../features/network";
-
+import merge from 'merge';
+import {CURRENCY_DATA} from "../../utils/currencies";
 
 export function *doFetchPrices(action) {
   try {
@@ -26,10 +27,15 @@ function *fetchAllTokenPrices() {
     let tokens = yield select(network.selectors.getTokens); // <-- get the project
     
     const symbols = Object.keys(tokens);
-    const fiat = ['USD', 'EUR', 'GBP', 'JPY', 'CNY', 'KRW']; // TODO: where will this list come from?
-
+    let data = {};
+    
     try {
-      const data = yield call(cc.priceMulti, symbols, fiat);
+      const size = 20;
+      for (let i = 0; i < CURRENCY_DATA.length; i += size) { // Query 20 currencies at a time
+        const fiat = CURRENCY_DATA.slice(i, i + size).map(x => x.id);
+        const prices = yield call(cc.priceMulti, symbols, fiat);
+        data = merge.recursive(true, prices, data);
+      }
       yield put({type: FETCH_PRICES_SUCCEEDED, data});
     } catch (error) {
       yield put({type: FETCH_PRICES_FAILED, error});
