0b660eec7f2c4a1cfa5448e21c93852b1892d4dc
Richard Ramos <info@richardramos.me>
2019-06-18 14:15:09 -0400

    fix: calculate percentages taking in account margin (#318)

--- src/js/pages/Escrow/components/EscrowDetail.jsx
+++ src/js/pages/Escrow/components/EscrowDetail.jsx
@@ -10,21 +10,33 @@ const PERCENTAGE_THRESHOLD = 10; // If asset price is 10% different than the rea
 
 const EscrowDetail = ({escrow, currentPrice}) => {
   const currentPriceForCurrency = parseFloat(currentPrice ? currentPrice[escrow.offer.currency] : null);
-  const escrowAssetPrice = escrow.assetPrice / 100;
-  return (<Row className="mt-4">
+  const escrowAssetPrice = escrow.assetPrice / 100 * ((escrow.offer.margin / 100) + 1);
+  const rateCurrentAndSellPrice = escrowAssetPrice / currentPriceForCurrency;
+
+  let isAbove = false;
+  let diffPercentage = 0;
+  if(rateCurrentAndSellPrice > 1){
+    diffPercentage = 100 - (1 / rateCurrentAndSellPrice * 100);
+  } else {
+    isAbove = true;
+    diffPercentage = 100 - (rateCurrentAndSellPrice * 100);
+  }
+
+return (<Row className="mt-4">
     <Col xs="2">
       <RoundedIcon icon={faQuestionCircle} bgColor="grey"/>
     </Col>
     <Col xs="10">
       <h5 className="m-0">Trade details</h5>
-      <p className="text-dark m-0">{(escrow.tokenAmount * escrow.assetPrice / 100).toFixed(2)} {escrow.offer.currency} for {escrow.tokenAmount} {escrow.token.symbol}</p>
-      <p className="text-dark m-0">{escrow.token.symbol} Price = {escrowAssetPrice} {escrow.offer.currency}</p>
+      <p className="text-dark m-0">{(escrow.tokenAmount * escrowAssetPrice).toFixed(2)} {escrow.offer.currency} for {escrow.tokenAmount} {escrow.token.symbol}</p>
+      <p className="text-dark m-0">{escrow.token.symbol} Price = {escrowAssetPrice.toFixed(2)} {escrow.offer.currency}</p>
       {escrow.expirationTime && escrow.expirationTime !== '0' && <p className="text-dark m-0">Expiration time: {moment(escrow.expirationTime * 1000).calendar()}</p>}
-      {escrow.status === tradeStates.waiting && currentPriceForCurrency && escrowAssetPrice * ((PERCENTAGE_THRESHOLD + 100) / 100) < currentPriceForCurrency &&
-      <Fragment>
-        <p className="text-danger font-weight-bold mb-0">The current price for {escrow.token.symbol} is {currentPriceForCurrency} {escrow.offer.currency}, which is {PERCENTAGE_THRESHOLD}% above the price for this trade</p>
+      {escrow.status === tradeStates.waiting && 
+       currentPriceForCurrency && diffPercentage > PERCENTAGE_THRESHOLD &&
+       <Fragment>
+        <p className="text-danger font-weight-bold mb-0">The current price for {escrow.token.symbol} is {currentPriceForCurrency} {escrow.offer.currency}, which is {diffPercentage.toFixed(2)}% {isAbove ? "above" : "below"} the price for this trade</p>
         <p className="text-danger mb-1">Double-check whether you really want to go through with this trade</p>
-      </Fragment>}
+      </Fragment> }
       </Col>
   </Row>);
 };
