03ebf43ea66b4a642282aa43ca1c2ff739807147
0x332 <kinspotting@protonmail.com>
2019-06-22 18:13:59 +0800

    add: ArbitratorLicense struct

--- contracts/teller-network/ArbitratorLicense.sol
+++ contracts/teller-network/ArbitratorLicense.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.5.8;
 import "./License.sol";
 
 contract ArbitratorLicense is License{
-	
+
+	struct ArbitratorLicenseDetails {
+        LicenseDetails licenseDetails;
+        bool acceptAny; // accept any seller
+        address[] accepted; // addresses of accepted sellers
+    }
+
+    mapping(address => ArbitratorLicenseDetails) arbitratorlicenseDetails;
+    
+    event ArbitratorLicensed(address buyer, uint256 price, bool acceptAny);
+
+
+
+
 }
