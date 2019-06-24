6f4d25519e9410206d9561d5d77399e94605598e
Richard Ramos <info@richardramos.me>
2019-06-24 15:51:32 -0400

    feat: adding proxy contracts

---
+++ contracts/proxy/Proxy.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "ProxyData.sol";
+
+contract Proxy is ProxyData {
+
+    constructor(address _proxied) public {
+        proxied = _proxied;
+    }
+
+    function () external payable {
+        address addr = proxied;
+        assembly {
+            let freememstart := mload(0x40)
+            calldatacopy(freememstart, 0, calldatasize())
+            let success := delegatecall(not(0), addr, freememstart, calldatasize(), freememstart, 32)
+            switch success
+            case 0 { revert(freememstart, 32) }
+            default { return(freememstart, 32) }
+        }
+    }
+
+}
