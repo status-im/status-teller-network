009e484b0f3eb83e228f83882d408dab1790f0df
Richard Ramos <info@richardramos.me>
2019-06-26 09:59:16 -0400

    refactor: provider update to take in account the relay contract

--- src/js/provider.js
+++ src/js/provider.js
@@ -1,12 +1,11 @@
-import Escrow from '../embarkArtifacts/contracts/Escrow';
+import EscrowRelay from '../embarkArtifacts/contracts/EscrowRelay';
 import {checkNotEnoughETH} from './utils/transaction';
-import {addressCompare} from './utils/address';
 
 const VALID_OPERATIONS = {
-  "cancel(uint256)": "40e58ee5",
-  "create(address,uint256,uint256,uint8,uint256,bytes,string,string)": "a63c5162",
-  "openCase(uint256,string)": "58b67904",
-  "pay(uint256)": "c290d691"
+  "cancel()": "ea8a1af0",
+  "create(uint256,uint256,uint8,uint256,bytes,string,string,uint256,bytes)": "b22bd55a",
+  "openCase(string)": "5cc80c53",
+  "pay()": "1b9265b8"
 };
 
 class Provider {
@@ -22,8 +21,7 @@ class Provider {
     const fSend = (payload, callback) => {
       const params = payload.params[0];
 
-      if (!(params && params.to && addressCompare(params.to, Escrow.options.address.toLowerCase()) &&
-        payload.method === "eth_sendTransaction" &&
+      if (!(payload.method === "eth_sendTransaction" &&
         Object.values(VALID_OPERATIONS).includes(params.data.substring(2, 10)))) {
         this.origProviderSend(payload, callback);
         return;
@@ -32,6 +30,7 @@ class Provider {
       (async () => {
         const balance = await web3.eth.getBalance(web3.eth.defaultAccount);
         const gasPrice = await web3.eth.getGasPrice();
+        
         if (checkNotEnoughETH(gasPrice, balance)) {
           // Increase 120%.
           // Normally we would use gaspriceFactorPercent on tabookey.RelayProvider
@@ -39,7 +38,26 @@ class Provider {
           // to set this gas price as a parameter. Tabookey will then use this value directly
           // without applying the factor percent
           const useGasPrice = web3.utils.toBN(gasPrice).mul(web3.utils.toBN("120")).div(web3.utils.toBN("100"));
+          const original_to = payload.params[0].to;
+          const funcSignature = payload.params[0].data.substring(0, 10);
+
           payload.params[0].gasPrice = web3.utils.toHex(useGasPrice);
+          payload.params[0].to = EscrowRelay.options.address;
+          payload.params[0].gas = web3.utils.fromDecimal(web3.utils.toDecimal(payload.params[0].gas) + 100000);
+
+          switch(funcSignature) {
+            case "0x1b9265b8": // pay()
+              payload.params[0].data = "0x0c11dedd000000000000000000000000" + original_to.substring(2); // pay(address);
+              break;
+            case "0xea8a1af0": // cancel()
+              payload.params[0].data = "0x4c33fe94000000000000000000000000" + original_to.substring(2); // cancel(address);
+              break;
+            case "0x5cc80c53": // openCase(string)
+              payload.params[0].data = "0xef37f1b6000000000000000000000000" + original_to.substring(2) + payload.params[0].data.substring(10); // openCase(address,string)
+              break;
+            default:
+          }
+
           this.relayProviderSend(payload, (error, result) => {
             callback(error, result);
           });
