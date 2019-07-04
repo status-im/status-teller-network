b6f75faf4ce439909640ae1ed0b0eb5c8d68f3e8
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-07-04 13:20:31 -0400

    feat: show a tooltip on offers you can't buy

--- src/js/pages/Profile/components/Offer.jsx
+++ src/js/pages/Profile/components/Offer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col, Button} from 'reactstrap';
+import {Row, Col, Button, UncontrolledTooltip} from 'reactstrap';
 import {truncateTwo} from '../../../utils/numbers';
 import {TokenImages} from '../../../utils/images';
 import {calculateEscrowPrice} from '../../../utils/transaction';
-
+import classnames from 'classnames';
 
 const Offer = ({offer, prices, onClick, disabled}) => (
   <Row className="border py-2 mx-0 my-2 rounded">
@@ -12,7 +12,12 @@ const Offer = ({offer, prices, onClick, disabled}) => (
       <p className="font-weight-bold"><img src={TokenImages[`${offer.token.symbol}.png`] || TokenImages[`generic.png`]} alt="asset icon" className="mr-2"/>{offer.token.symbol}</p>
     </Col>
     <Col xs="8" className="v-align-center text-right">
-      <Button color={disabled ? "secondary" : "primary" } className="p-2" disabled={disabled} onClick={onClick}>Buy for {truncateTwo(calculateEscrowPrice(offer, prices))} {offer.currency}</Button>
+      <Button color={disabled ? "secondary" : "primary" } className={classnames('p-2', {disabled, 'not-clickable': disabled})} onClick={disabled ? null : onClick} id={`buy-btn-${offer.id}`}>
+        Buy for {truncateTwo(calculateEscrowPrice(offer, prices))} {offer.currency}
+      </Button>
+      {<UncontrolledTooltip placement="top" target={`buy-btn-${offer.id}`}>
+        Offer disabled because you are the seller or the arbitrator
+      </UncontrolledTooltip>}
     </Col>
   </Row>
 );
