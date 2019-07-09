31c305f02fcd3319e6f17043335cce4281b2b488
Richard Ramos <info@richardramos.me>
2019-07-09 10:32:28 -0400

    feat: add arbitrator info to escrow details (#361)
    * feat: add arbitrator details to escrow page
    * feat: add address to arbitrator format

--- src/js/utils/strings.js
+++ src/js/utils/strings.js
@@ -1,5 +1,6 @@
+import {compactAddress} from "./address";
 
 export const formatArbitratorName = (user, address, noUsernameLabel, index) => {
   if (!user) return address + ' - Loading...';
-  return `${index >= 0 ? index + 1 + ' - ' : ""}${user.username || (noUsernameLabel ? noUsernameLabel : "No username available") }${user.location ? ' from ' + user.location : ''} - ${user.upCount || 0}↑  ${user.downCount || 0}↓`;
+  return `${index >= 0 ? index + 1 + ' - ' : ""}${user.username || (noUsernameLabel ? noUsernameLabel : "No username available")} (${compactAddress(address, 3)})  ${user.location ? ' from ' + user.location : ''} - ${user.upCount || 0}↑  ${user.downCount || 0}↓`;
 };
