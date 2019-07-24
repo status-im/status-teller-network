f3ee8117b03f70369fd6c61d019cf07119fa5874
Richard Ramos <info@richardramos.me>
2019-07-24 13:51:47 -0400

    fix: escrow warnings for buyers and sellers (#387)
    * fix: proper escrow warnings for buyers and sellers
    * fix: show warning only if it's negative for the user

--- src/js/pages/Escrow/components/EscrowDetail.jsx
+++ src/js/pages/Escrow/components/EscrowDetail.jsx
@@ -11,19 +11,36 @@ import { zeroAddress } from '../../../utils/address';
 
 const PERCENTAGE_THRESHOLD = 10; // If asset price is 10% different than the real price, show the warning
 
-const EscrowDetail = ({escrow, currentPrice}) => {
-  const currentPriceForCurrency = parseFloat(currentPrice ? currentPrice[escrow.offer.currency] : null).toFixed(2);
-  const escrowAssetPrice = escrow.assetPrice / 100 * ((escrow.offer.margin / 100) + 1);
-  const rateCurrentAndSellPrice = escrowAssetPrice / currentPriceForCurrency;
-
+const getDiff = (rate) => {
   let isAbove = false;
   let diffPercentage = 0;
-  if(rateCurrentAndSellPrice > 1){
-    diffPercentage = 100 - (1 / rateCurrentAndSellPrice * 100);
-  } else {
+  if(rate > 1) {
     isAbove = true;
-    diffPercentage = 100 - (rateCurrentAndSellPrice * 100);
+    diffPercentage = (rate * 100) - 100;
+  } else {
+    diffPercentage = 100 - (rate * 100);
   }
+
+  return {isAbove, diffPercentage};
+};
+
+
+const shouldWarn = (diff, isBuyer) => {
+  if(isBuyer && !diff.isAbove && diff.diffPercentage > PERCENTAGE_THRESHOLD) return true;
+  if(!isBuyer && diff.isAbove && diff.diffPercentage > PERCENTAGE_THRESHOLD) return true;
+  return false;
+};
+
+
+const EscrowDetail = ({escrow, currentPrice, isBuyer}) => {
+  const currentPriceForCurrency = parseFloat(currentPrice ? currentPrice[escrow.offer.currency] : null).toFixed(2);
+  const currentOfferPrice = currentPriceForCurrency * ((escrow.offer.margin / 100) + 1);
+  const escrowAssetPrice = escrow.assetPrice / 100 * ((escrow.offer.margin / 100) + 1);
+  const rateCurrentAndSellPrice = currentPriceForCurrency / escrowAssetPrice;
+  const rateCurrentAndBuyerPrice = currentOfferPrice / escrowAssetPrice;
+
+  const buyerDiff = getDiff(rateCurrentAndSellPrice);
+  const sellerDiff = getDiff(rateCurrentAndBuyerPrice);
 
   return (<Row className="mt-4">
     <Col xs="2">
@@ -34,12 +51,18 @@ const EscrowDetail = ({escrow, currentPrice}) => {
       <p className="text-dark m-0">{(escrow.tokenAmount * escrowAssetPrice).toFixed(2)} {escrow.offer.currency} for {escrow.tokenAmount} {escrow.token.symbol}</p>
       <p className="text-dark m-0">{escrow.token.symbol} Price = {escrowAssetPrice.toFixed(2)} {escrow.offer.currency}</p>
       {escrow.expirationTime && escrow.expirationTime !== '0' && <p className="text-dark m-0">Expiration time: {moment(escrow.expirationTime * 1000).calendar()}</p>}
-      {escrow.status === tradeStates.waiting &&
-       currentPriceForCurrency && diffPercentage > PERCENTAGE_THRESHOLD &&
+      
+      {escrow.status === tradeStates.waiting && isBuyer && currentPriceForCurrency && shouldWarn(buyerDiff, true) &&
        <Fragment>
-        <p className="text-danger font-weight-bold mb-0">The current price for {escrow.token.symbol} is {currentPriceForCurrency} {escrow.offer.currency}, which is {diffPercentage.toFixed(2)}% {isAbove ? "above" : "below"} the price for this trade</p>
+        <p className="text-danger font-weight-bold mb-0">The current price for {escrow.token.symbol} is {currentPriceForCurrency} {escrow.offer.currency}, which is {buyerDiff.diffPercentage.toFixed(2)}% {buyerDiff.isAbove ? "above" : "below"} the price for this trade ({escrowAssetPrice.toFixed(2)} {escrow.offer.currency})</p>
         <p className="text-danger mb-2">Double-check whether you really want to go through with this trade</p>
       </Fragment> }
+
+      {escrow.status === tradeStates.waiting && !isBuyer && currentPriceForCurrency  && shouldWarn(sellerDiff, false)  &&
+      <Fragment>
+      <p className="text-danger font-weight-bold mb-0">Your current price for {escrow.token.symbol} in this offer is {currentOfferPrice.toFixed(2)} {escrow.offer.currency} which is {sellerDiff.diffPercentage.toFixed(2)}% {sellerDiff.isAbove ? "above" : "below"} the price for this trade ({escrowAssetPrice.toFixed(2)} {escrow.offer.currency})</p>
+      <p className="text-danger mb-2">Double-check whether you really want to go through with this trade</p>
+    </Fragment> }
       <NoArbitratorWarning arbitrator={escrow.offer.arbitrator} />
       { escrow.arbitrator !== zeroAddress && <p className="text-dark m-0">Arbitrator: <span className="font-weight-bold">{formatArbitratorName(escrow.arbitratorInfo, escrow.arbitrator)}</span></p>}
       </Col>
@@ -49,7 +72,8 @@ const EscrowDetail = ({escrow, currentPrice}) => {
 
 EscrowDetail.propTypes = {
   escrow: PropTypes.object,
-  currentPrice: PropTypes.object
+  currentPrice: PropTypes.object,
+  isBuyer: PropTypes.bool
 };
 
 export default EscrowDetail;
