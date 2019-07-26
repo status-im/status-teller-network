4e3aad513763c7164efb68c318bf58f48fff3514
Richard Ramos <info@richardramos.me>
2019-07-26 08:58:07 -0400

    feat: Profile button v1.1 (#399)
    * feat: Profile button v1.1

--- src/js/layout/Header/index.jsx
+++ src/js/layout/Header/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import PropTypes from 'prop-types';
 import {Navbar, NavbarBrand, Nav, NavLink, NavItem} from 'reactstrap';
 
 import "./index.scss";
 import logo from "../../../images/logo-small.svg";
+import iconProfile from "../../../images/profile.svg";
+import iconCloseProfile from "../../../images/close_profile.svg";
 
 
-const Header = ({profile}) => (
+const Header = ({profile, location}) => (
   <header className="border-bottom">
     <Navbar expand="md" className="px-0">
       <NavbarBrand tag={Link} to="/"><img src={logo} alt="Logo" width="32" height="32" /><span className="text-body text-logo">TN</span></NavbarBrand>
       <Nav className="ml-auto" navbar>
         <NavItem>
-          {profile && <NavLink tag={Link} to="/profile">Profile</NavLink>}
+          {profile && location.pathname !== '/profile' && <NavLink tag={Link} to="/profile"><img src={iconProfile} alt="Profile" width="32" height="32" /></NavLink>}
+          {profile && location.pathname === '/profile' && <NavLink tag={Link} to="/"><img src={iconCloseProfile} alt="Home" width="32" height="32" /></NavLink>}
         </NavItem>
       </Nav>
     </Navbar>
@@ -22,7 +25,8 @@ const Header = ({profile}) => (
 
 Header.propTypes = {
   history: PropTypes.object,
-  profile: PropTypes.object
+  profile: PropTypes.object,
+  location: PropTypes.object
 };
 
-export default Header;
+export default withRouter(Header);
