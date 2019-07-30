bcfc3a63dcc5b28b4740e2b39672ce3ca0225dfa
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-07-30 12:53:19 -0400

    remove useless variable

--- src/js/components/Offer/index.jsx
+++ src/js/components/Offer/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {Card, CardBody, CardTitle, CardFooter} from 'reactstrap';
 import {Link} from "react-router-dom";
