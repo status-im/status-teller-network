ae29a146d66110cc29b145fd1938b274d4df89ef
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-08-01 11:51:40 -0400

    feat: make payment methods actually check boxes

--- src/js/ui/CheckButton/index.jsx
+++ src/js/ui/CheckButton/index.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Button} from 'reactstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircle} from "@fortawesome/free-solid-svg-icons";
+import {faCircle, faCheck} from "@fortawesome/free-solid-svg-icons";
 import {faCircle as faCircleReg} from "@fortawesome/free-regular-svg-icons";
 import classnames from 'classnames';
 
 import "./index.scss";
 
-const CheckButton = ({children, active, onClick, size, align}) => (
+const CheckButton = ({children, active, onClick, size, align, isCheckBox}) => (
   <Button className={classnames('check-button', 'text-left', 'text-body', 'px-0', 'py-2', 'v-align-center', 'mb-2', {
     large: size === 'l',
     small: size === 's'
   })} size="lg" color="link" block active={active} onClick={onClick}>
     {children}
-    <FontAwesomeIcon className={classnames({"float-right": align === "right", "float-left": align !== "right", "text-primary": active, "text-secondary": !active})}
-                     icon={active ? faCircleReg : faCircle}/>
+    {!isCheckBox && <FontAwesomeIcon className={classnames({"float-right": align === "right", "float-left": align !== "right", "text-primary": active, "text-secondary": !active})}
+                     icon={active ? faCircleReg : faCircle}/>}
+    {isCheckBox && <span className={classnames("d-inline-block check-box text-center", {'bg-secondary': !active, 'bg-primary': active, "float-right": align === "right", "float-left": align !== "right"})}>
+      {active && <FontAwesomeIcon className="text-white" icon={faCheck}/>}
+    </span>}
   </Button>
 );
 
@@ -30,6 +33,7 @@ CheckButton.propTypes = {
   ]),
   align: PropTypes.string,
   active: PropTypes.bool,
+  isCheckBox: PropTypes.bool,
   onClick: PropTypes.func,
   size: PropTypes.string
 };
