84f09a2942a39f74cf3281d0fd3892a5262915e0
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-08-19 09:19:40 -0400

    fix user check icon (#436)

--- src/js/pages/Escrow/components/ReleaseFunds.jsx
+++ src/js/pages/Escrow/components/ReleaseFunds.jsx
@@ -2,16 +2,16 @@ import React, {Fragment} from 'react';
 import {Row, Col, Button} from 'reactstrap';
 import RoundedIcon from "../../../ui/RoundedIcon";
 import PropTypes from 'prop-types';
-import {faUserCheck} from "@fortawesome/free-solid-svg-icons";
 import classnames from 'classnames';
 import ArrowDown from "../../../../images/down-arrow.svg";
+import UserCheck from "../../../../images/user-check.svg";
 import CheckIcon from "../../../../images/check.svg";
 
 // eslint-disable-next-line complexity
 const ReleaseFunds = ({isBuyer, isActive, isDone, action, isPaid, disabled}) => (
   <Row className="mt-4">
     <Col xs="1">
-      {!isDone && <RoundedIcon size="xs" icon={faUserCheck} bgColor={isActive ? "primary" : "grey"}/>}
+      {!isDone && <RoundedIcon size="xs" image={UserCheck} bgColor={isActive ? "primary" : "grey"}/>}
       {isDone && <RoundedIcon size="xs" image={CheckIcon} bgColor="green"/>}
       {!isDone && isActive && <img src={ArrowDown} alt="" className="arrow" />}
     </Col>
