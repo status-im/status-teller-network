fad4bb01c7694aa6cfbcf70c2a584fd37ae9a934
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-11-11 11:05:23 -0500

    fix: fix profile page and header when not on profile

--- src/js/components/NoLicense/index.jsx
+++ src/js/components/NoLicense/index.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import {Button} from 'reactstrap';
 import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
-import './index.scss';
 
 const NoLicense = ({arbitratorPage}) => (
-<div className="no-license">
+<div className="no-license mt-5 px-3">
     {arbitratorPage && <h3>You don&apos;t have an arbitrator license </h3>}
     {!arbitratorPage && <h3>You don&apos;t have a seller license </h3>}
     {arbitratorPage && <p className="text-muted">Once you are approved as an arbitrator you can make tokens by judging disputes.</p> }
     {!arbitratorPage && <p className="text-muted">Once you are a seller you will be able to create offers</p> }
-    <p className="mt-5 pt-5 text-center">
-        {arbitratorPage && <Button color="primary" tag={Link} to="/arbitrator/license" className="mt-5">Become an arbitrator</Button> }
-        {!arbitratorPage && <Button color="primary" tag={Link} to="/license" className="mt-5">Become a seller</Button> }
+    <p className="mt-5 text-center">
+        {arbitratorPage && <Button color="primary" tag={Link} to="/arbitrator/license">Become an arbitrator</Button> }
+        {!arbitratorPage && <Button color="primary" tag={Link} to="/license">Become a seller</Button> }
     </p>
 </div>
 );
@@ -24,6 +23,6 @@ NoLicense.defaultProps = {
 NoLicense.propTypes = {
     arbitratorPage: PropTypes.bool
   };
-  
+
 
 export default NoLicense;
