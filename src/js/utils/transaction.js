4b5dcc0858f4b3995a8b0571514360e23022564f
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-11-18 15:17:03 -0500

    feat: new design for the my trades page

--- src/js/utils/transaction.js
+++ src/js/utils/transaction.js
@@ -14,9 +14,8 @@ export const calculateEscrowPrice = (escrow, prices) => {
   return prices[escrow.token.symbol][escrow.currency] * ((100 + (parseFloat(escrow.margin))) / 100);
 };
 
-const toBN = web3.utils.toBN;
-
 export const checkNotEnoughETH = (gasPrice, ethBalance) => {
+  const toBN = web3.utils.toBN;
   const relayGasPrice = toBN(gasPrice || '0').mul(toBN(120)).div(toBN(100)); // 120%. toBN doesnt like decimals?
   return toBN(web3.utils.toWei(ethBalance || '0', 'ether')).lt(toBN(600000).mul(relayGasPrice)); // only allow ETH if less than 500000*gasPrice
 };
