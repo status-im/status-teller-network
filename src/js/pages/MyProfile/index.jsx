5c0d68b833e339553107551f4a7ec112f59d0988
Richard Ramos <info@richardramos.me>
2019-11-29 08:00:37 -0400

    fix: do not redirect to contact data if the user is new to teller (#515)

--- src/js/pages/MyProfile/index.jsx
+++ src/js/pages/MyProfile/index.jsx
@@ -55,7 +55,7 @@ class MyProfile extends Component {
       this.load();
     }
 
-    if(this.props.profile && !this.props.profile.contactData){
+    if(this.props.profile && this.props.profile.isArbitrator && !this.props.profile.contactData){
       return this.props.history.push("/profile/settings/contact");
     }
 
