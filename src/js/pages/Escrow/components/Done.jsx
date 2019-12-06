9dfbc5134ba1754e01fc8f9506ca5d10bcebb7ce
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-12-06 10:30:19 -0500

    fix: fix weird rating ui

--- src/js/pages/Escrow/components/Done.jsx
+++ src/js/pages/Escrow/components/Done.jsx
@@ -8,11 +8,10 @@ import CheckIcon from "../../../../images/check.svg";
 import RatingIcon from "../../../ui/RatingIcon";
 import classnames from 'classnames';
 
+// eslint-disable-next-line complexity
 const Done = ({isDone, hadDispute, isBuyer, isActive, trade, rateSellerStatus, rateBuyerStatus, rateTransaction}) => {
-  const rating = 5;
-  // const rating = trade ? parseInt(isBuyer ? trade.sellerRating : trade.buyerRating, 10) : 0;
-  const tradeWasRated = true;
-  // const tradeWasRated = trade && rating !== 0;
+  const rating = trade ? parseInt(isBuyer ? trade.sellerRating : trade.buyerRating, 10) : 0;
+  const tradeWasRated = trade && rating !== 0;
 
   return (
   <Row className="mt-4">
@@ -33,51 +32,34 @@ const Done = ({isDone, hadDispute, isBuyer, isActive, trade, rateSellerStatus, r
       {isDone && <p className="m-0 text-muted text-small">Trade is complete</p>}
     </Col>
     {isActive && !hadDispute && <Col xs="5" sm="3">
-      <div className="rounded p-2 position-relative shadow-sm bg-white">
-        <p className="mb-1 text-small text-black">How did the trade go?</p>
+      <div className={classnames("rounded p-2 position-relative bg-white", {'shadow-sm': !tradeWasRated})}>
+        {!tradeWasRated && <p className="mb-1 text-small text-black">How did the trade go?</p>}
         <p className="m-0 text-center">
-          {isBuyer && <Reputation trade={trade}
+          {tradeWasRated &&
+          <span
+            className={classnames("rounded-circle rating-with-action rating-rated float-right mr-3 mt-2 text-center", {
+              'bg-success': rating > 3,
+              'bg-danger': rating <= 3
+            })}>
+            <RatingIcon isPositiveRating={rating > 3} isRated={true} size="sm"/>
+          </span>
+          }
+          {!tradeWasRated && isBuyer &&
+          <Reputation trade={trade}
                       rateTransaction={(rateSellerStatus !== States.pending && rateSellerStatus !== States.success) ? rateTransaction : null}
                       size="l"
                       isBuyer={isBuyer}
-                      />}
-          {!isBuyer && <Reputation trade={trade}
+          />}
+          {!tradeWasRated && !isBuyer &&
+          <Reputation trade={trade}
                       rateTransaction={(rateBuyerStatus !== States.pending && rateBuyerStatus !== States.success) ? rateTransaction : null}
                       size="l"
                       isBuyer={isBuyer}
-                      />}
+          />}
         </p>
-
-        {!isDone && <p className="m-0 text-muted text-small">
-          Trade is finished once every step above is complete
-        </p>}
-
-        {isDone && <p className="m-0 text-muted text-small">Trade is complete</p>}
-        </div>
-      </Col>}
-      {isActive && <Col xs="5" sm="3">
-        {tradeWasRated &&
-          <span className={classnames("rounded-circle rating-with-action rating-rated float-right mr-3 mt-2 text-center", {'bg-success': rating > 3, 'bg-danger': rating <= 3})}>
-            <RatingIcon isPositiveRating={rating > 3} isRated={true} size="sm"/>
-          </span>
-        }
-        {!tradeWasRated && <div className="rounded p-2 position-relative shadow-sm bg-white">
-          <p className="mb-1 text-small text-black">How did the trade go?</p>
-          <p className="m-0 text-center">
-            {isBuyer && <Reputation trade={trade}
-                                    rateTransaction={(rateSellerStatus !== States.pending && rateSellerStatus !== States.success) ? rateTransaction : null}
-                                    size="l"
-                                    isBuyer={isBuyer}
-            />}
-            {!isBuyer && <Reputation trade={trade}
-                                     rateTransaction={(rateBuyerStatus !== States.pending && rateBuyerStatus !== States.success) ? rateTransaction : null}
-                                     size="l"
-                                     isBuyer={isBuyer}
-            />}
-          </p>
-        </div>}
-      </Col>}
-    </Row>
+      </div>
+    </Col>}
+  </Row>
   );
 };
 
