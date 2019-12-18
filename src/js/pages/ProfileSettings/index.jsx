c663d3ffbb3cd40ab505467d941cde41b33c8316
Richard Ramos <info@richardramos.me>
2019-12-18 13:25:05 -0400

    fix: change location flag to a pin (#570)
    * fix: change location flag to a pin
    
    * Deleting USA flag

--- src/js/pages/ProfileSettings/index.jsx
+++ src/js/pages/ProfileSettings/index.jsx
@@ -5,12 +5,12 @@ import ProfileButton from "../MyProfile/components/ProfileButton";
 import iconChat from "../../../images/read-chat.svg";
 import iconBell from "../../../images/bell.svg";
 import iconSettings from "../../../images/settings.svg";
-import { ReactComponent as iconFlag } from "../../../images/flag.svg";
+import { ReactComponent as iconLocation } from "../../../images/location.svg";
 
 const ProfileSettings = ({t}) => (<Fragment>
   <h2 className="mb-4 mt-3">{t('profileSettings.title')}</h2>
   <ProfileButton linkTo="/profile/settings/contact" image={iconChat} title={t('profileSettings.contactInfo')}/>
-  <ProfileButton linkTo="/profile/settings/location" imageComponent={iconFlag} title={t('profileSettings.location')}/>
+  <ProfileButton linkTo="/profile/settings/location" imageComponent={iconLocation} title={t('profileSettings.location')}/>
   <ProfileButton linkTo="/profile/settings/notifications" image={iconBell} title={t('profileSettings.notifications')}/>
   <ProfileButton linkTo="/profile/settings/cache" image={iconSettings} title={t('profileSettings.cache')}/>
 </Fragment>);
