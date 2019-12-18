9fbe5056ea39c4600dc5b8d536f72fe81367f8c4
Richard Ramos <info@richardramos.me>
2019-12-18 13:24:42 -0400

    feat: chat with arbitrator (#568)

--- src/js/pages/Escrow/components/EscrowDetail.jsx
+++ src/js/pages/Escrow/components/EscrowDetail.jsx
@@ -1,14 +1,15 @@
 import React, {Fragment} from 'react';
-import {Row, Col} from 'reactstrap';
+import {Row, Col, Button} from 'reactstrap';
 import PropTypes from 'prop-types';
 import moment from "moment";
 import Identicon from "../../../components/UserInformation/Identicon";
-import {formatArbitratorName, renderContactData} from '../../../utils/strings';
+import {formatArbitratorName, renderContactData,stringToContact} from '../../../utils/strings';
 import {PAYMENT_METHODS} from '../../../features/metadata/constants';
 import PriceWarning from '../../../components/PriceWarning';
+import Address from '../../../components/UserInformation/Address';
 import {withTranslation} from "react-i18next";
 
-const EscrowDetail = ({t, escrow, currentPrice, isBuyer}) => {
+const EscrowDetail = ({t, escrow, currentPrice, isBuyer, arbitrationDetails, onClickChat}) => {
   if(!escrow.seller || !escrow.buyerInfo || !escrow.arbitratorInfo) return null;
 
   const escrowAssetPrice = (escrow.fiatAmount / 100) / escrow.tokenAmount;
@@ -39,31 +40,67 @@ const EscrowDetail = ({t, escrow, currentPrice, isBuyer}) => {
 
     {isBuyer && <Fragment>
       <h3 className="mt-4 font-weight-normal">{t('general.seller')}</h3>
-      <p className="mt-2 font-weight-medium mb-1">
-        <Identicon seed={escrow.offer.owner} className="rounded-circle border mr-2 float-left" scale={5}/>
-        {escrow.seller.username}
-      </p>
-      {renderContactData(escrow.seller.contactData)}
+      <Row noGutters>
+        <Col xs={9}>
+          <div className="mt-2 font-weight-medium">
+            <Identicon seed={escrow.offer.owner} className="rounded-circle border mr-2 float-left mb-5" scale={5}/>
+            {escrow.seller.username}
+            {renderContactData(escrow.seller.contactData, 'mb-0')}
+            <p className="text-muted text-small addr m-0">
+              {t('general.address')}: <Address disableHover address={escrow.offer.owner} length={6}/>
+            </p>
+          </div>
+        </Col>
+        <Col xs={3} className="pt-3">
+          <Button color="primary" size="sm" onClick={onClickChat(isBuyer ? 'seller' : 'buyer')}>
+            {t('escrow.openChat.chat')}
+          </Button>
+        </Col>
+      </Row>
     </Fragment>}
 
     {!isBuyer && <Fragment>
       <h3 className="mt-4 font-weight-normal">{t('general.buyer')}</h3>
-      <p className="mt-2 font-weight-medium mb-1">
-        <Identicon seed={escrow.buyer} className="rounded-circle border mr-2 float-left" scale={5}/>
-        {escrow.buyerInfo.username}
-      </p>
-      {renderContactData(escrow.buyerInfo.contactData)}
+      <Row noGutters>
+        <Col xs={9}>
+          <div className="mt-2 font-weight-medium">
+            <Identicon seed={escrow.buyer} className="rounded-circle border mr-2 float-left mb-5" scale={5}/>
+            {escrow.buyerInfo.username}
+            {renderContactData(escrow.buyerInfo.contactData, 'mb-0')}
+            <p className="text-muted text-small addr m-0">
+              {t('general.address')}: <Address disableHover address={escrow.buyer} length={6}/>
+            </p>
+          </div>
+        </Col>
+        <Col xs={3} className="pt-3">
+          <Button color="primary" size="sm" onClick={onClickChat(isBuyer ? 'seller' : 'buyer')}>
+            {t('escrow.openChat.chat')}
+          </Button>
+        </Col>
+      </Row>
     </Fragment>}
 
-      {escrow.arbitratorInfo && <Fragment>
-        <h3 className="mt-4 font-weight-normal">{t('general.arbitrator')}</h3>
-        <p className="mt-2 font-weight-medium mb-1">
-          <Identicon seed={escrow.arbitrator} className="rounded-circle border mr-2 float-left" scale={5}/>
-          {formatArbitratorName(escrow.arbitratorInfo, escrow.arbitrator)}
-        </p>
-        {renderContactData(escrow.arbitratorInfo.contactData)}
-      </Fragment>
-      }
+    {escrow.arbitratorInfo && <Fragment>
+      <h3 className="font-weight-normal">{t('general.arbitrator')}</h3>
+      <Row noGutters>
+        <Col xs={9}>
+          <div className="mt-2 font-weight-medium">
+            <Identicon seed={escrow.arbitrator} className="rounded-circle border mr-2 float-left mb-5" scale={5}/>
+            {formatArbitratorName(escrow.arbitratorInfo, escrow.arbitrator)}
+            {arbitrationDetails.open && renderContactData(escrow.arbitratorInfo.contactData, 'mb-0') }
+            {!arbitrationDetails.open && <p className="text-muted text-small m-0">{t('general.contactMethod')}: {stringToContact(escrow.arbitratorInfo.contactData).method}</p> }
+            <p className="text-muted text-small addr m-0">
+              {t('general.address')}: <Address disableHover address={escrow.arbitrator} length={6}/>
+            </p>
+          </div>
+        </Col>
+        <Col xs={3}>
+          <Button color={arbitrationDetails.open ? 'primary': 'secondary'} disabled={!arbitrationDetails.open} size="sm" onClick={onClickChat('arbitrator')}>
+            {t('escrow.openChat.chat')}
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>}
 
     <Row className="mt-4">
       <Col xs="12">
@@ -88,9 +125,11 @@ const EscrowDetail = ({t, escrow, currentPrice, isBuyer}) => {
 
 EscrowDetail.propTypes = {
   t: PropTypes.func,
+  arbitrationDetails: PropTypes.object,
   escrow: PropTypes.object,
   currentPrice: PropTypes.object,
-  isBuyer: PropTypes.bool
+  isBuyer: PropTypes.bool,
+  onClickChat: PropTypes.func
 };
 
 export default withTranslation()(EscrowDetail);
