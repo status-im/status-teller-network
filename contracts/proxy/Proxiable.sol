b7ff83186fbd0b7783a4484b60897fe69e1009f3
Richard Ramos <info@richardramos.me>
2019-12-19 14:09:17 -0400

    feat: Use EIP1822 proxy

---
+++ contracts/proxy/Proxiable.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Proxiable {
+    // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+    event Upgraded(address indexed implementation);
+
+    function updateCodeAddress(address newAddress) internal {
+        require(
+            bytes32(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) == Proxiable(newAddress).proxiableUUID(),
+            "Not compatible"
+        );
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, newAddress)
+        }
+        emit Upgraded(newAddress);
+    }
+    function proxiableUUID() public pure returns (bytes32) {
+        return 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7;
+    }
+}
--- contracts/proxy/Proxy.sol
+++ contracts/proxy/Proxy.sol
@@ -3,39 +3,51 @@
 
 pragma solidity >=0.5.0 <0.6.0;
 
-// Adapted from
-// https://github.com/zeppelinos/labs/tree/master/upgradeability_using_unstructured_storage
-
 /**
  * @title Proxy
- * @dev Basic EIP897 proxy functionality
+ * @dev EIP1822
  */
 contract Proxy {
+    constructor(bytes memory constructData, address contractLogic) public {
+        // save the code address
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
+        }
+
+        if (constructData.length == 0) {
+            return;
+        }
+
+        (bool success, bytes memory _) = contractLogic.delegatecall(constructData); // solium-disable-line
+        require(success, "Construction failed");
+    }
 
     /**
      * @dev Tells the address of the implementation where every call will be delegated.
      * @return address of the implementation to which it will be delegated
      */
-    function implementation() public view returns (address);
+    function implementation() public view returns (address) {
+        return address(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7);
+    }
 
     /**
      * @dev Fallback function allowing to perform a delegatecall to the given implementation.
      * This function will return whatever the implementation call returns
      */
-    function () external payable {
-        address _impl = implementation();
-        require(_impl != address(0), "No implementation available");
-
-        assembly {
-            let ptr := mload(0x40)
-            calldatacopy(ptr, 0, calldatasize)
-            let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
-            let size := returndatasize
-            returndatacopy(ptr, 0, size)
-
-            switch result
-            case 0 { revert(ptr, size) }
-            default { return(ptr, size) }
+    function() external payable {
+        assembly { // solium-disable-line
+            let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+            calldatacopy(0x0, 0x0, calldatasize)
+            let success := delegatecall(sub(gas, 10000), contractLogic, 0x0, calldatasize, 0, 0)
+            let retSz := returndatasize
+            returndatacopy(0, 0, retSz)
+            switch success
+            case 0 {
+                revert(0, retSz)
+            }
+            default {
+                return(0, retSz)
+            }
         }
     }
 
