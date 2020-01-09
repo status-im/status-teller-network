2ba1b5569d65fc09010e5442e918dc696e1c353b
Richard Ramos <info@richardramos.me>
2020-01-09 15:04:16 -0400

    fix: hide price warning on escrow page

--- src/js/components/PriceWarning/index.jsx
+++ src/js/components/PriceWarning/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 import React, {Fragment} from 'react';
 import {tradeStates} from '../../features/escrow/helpers';
 import RoundedIcon from "../../ui/RoundedIcon";
@@ -10,7 +11,7 @@ import {withTranslation} from "react-i18next";
 
 const PERCENTAGE_THRESHOLD = 10; // If asset price is 10% different than the real price, show the warning
 
-const PriceWarning = ({t, isBuyer = true, escrowStatus = tradeStates.waiting, fiatAmount, tokenAmount, tokenSymbol, fiatSymbol, margin = 0, currentPrice = '0.00'}) => {
+const PriceWarning = ({t, hideDefaultText, isBuyer = true, escrowStatus = tradeStates.waiting, fiatAmount, tokenAmount, tokenSymbol, fiatSymbol, margin = 0, currentPrice = '0.00'}) => {
   const currentPriceForCurrency = parseFloat(currentPrice).toFixed(4);
   const currentOfferPrice = currentPriceForCurrency * ((margin / 100) + 1);
   const escrowAssetPrice = (fiatAmount / 100) / tokenAmount;
@@ -32,11 +33,12 @@ const PriceWarning = ({t, isBuyer = true, escrowStatus = tradeStates.waiting, fi
           currentPrice: limitDecimals(currentPriceForCurrency, 4)
         })}
       </p>}
-      <p className={classnames("text-small", {"text-danger": shouldWarnBuyer, "text-muted": !shouldWarnBuyer})}>
+      
+      {(!hideDefaultText || shouldWarnBuyer) && <p className={classnames("text-small", {"text-danger": shouldWarnBuyer, "text-muted": !shouldWarnBuyer})}>
         <RoundedIcon image={shouldWarnBuyer ? infoIconRed : infoIconGray}
                      bbgColor={shouldWarnBuyer ? "red" : "secondary"} className="float-left mr-1" size="sm"/>
         <span className="pt-2">{t('priceWarning.onlyContinueIf')}</span>
-      </p>
+      </p>}
     </Fragment>}
 
     {escrowStatus === tradeStates.waiting && !isBuyer && currentPriceForCurrency &&
@@ -50,17 +52,25 @@ const PriceWarning = ({t, isBuyer = true, escrowStatus = tradeStates.waiting, fi
           currentPrice: limitDecimals(currentPriceForCurrency, 4)
         })}
       </p>}
-      <p className={classnames("text-small", {"text-danger": shouldWarnSeller, "text-muted": !shouldWarnSeller})}>
+
+      
+      {(!hideDefaultText || shouldWarnSeller) && <p className={classnames("text-small", {"text-danger": shouldWarnSeller, "text-muted": !shouldWarnSeller})}>
         <RoundedIcon image={shouldWarnSeller ? infoIconRed : infoIconGray}
                      bbgColor={shouldWarnSeller ? "red" : "secondary"} className="float-left mr-1" size="sm"/>
         <span className="pt-2">{t('priceWarning.onlyContinueIf')}</span>
-      </p>
+      </p>}
+
     </Fragment>}
   </Fragment>;
 };
 
+PriceWarning.defaultProps = {
+  hideDefaultText: false
+};
+
 PriceWarning.propTypes = {
   t: PropTypes.func,
+  showDefaultText: PropTypes.bool,
   isBuyer: PropTypes.bool,
   escrowStatus: PropTypes.string,
   fiatAmount: PropTypes.oneOfType([
