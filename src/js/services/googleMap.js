341bf23989a215b569dbbba82ee6c404e582fd44
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-01-16 18:02:04 -0500

    fix: use geocoding client side API instead of server side one (#629)

--- src/js/services/googleMap.js
+++ src/js/services/googleMap.js
@@ -1,21 +1,21 @@
-export const API_KEY = 'AIzaSyBudXYUykxdQZ7ljEdw9wCBpGNzofNWs7Q';
-
-const geocoder = require('google-geocoder');
-const geo = geocoder({
-  key: API_KEY
-});
+/*global google*/
+export const API_KEY = 'AIzaSyBudXYUykxdQZ7ljEdw9wCBpGNzofNWs7Q'; // Caution: this is used in index.html too
+const geocoder = new google.maps.Geocoder();
 
 export function getLocation(place) {
   return new Promise((resolve, reject) => {
 
-    geo.find(place, function(err, res) {
-      if (err) {
-        return reject(err);
+    geocoder.geocode({address: place}, (results, status) => {
+      if (status === 'OK') {
+        resolve({
+          location: {
+            latitude: results[0].geometry.location.lat(),
+            longitude: results[0].geometry.location.lng()
+          }, countryCode: results[0].address_components[3].short_name
+        });
+      } else {
+        return reject(new Error(`No result for location ${place}. Status: ${status}`));
       }
-      if (!res.length) {
-        return reject(new Error(`No result for location ${place}`));
-      }
-      resolve({location: res[0].location, countryCode: res[0].country.short_name});
     });
   });
 }
