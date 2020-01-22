580d70e370353bef52cf834d87fa13faaf63bd74
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-01-22 09:02:46 -0500

    Couple of fixes to the filters (#648)
    * feat: add clear button to token filter
    
    * fix: fix currency modal to not exit on backspace and save on enter
    
    * fix: fix filter separator position

--- src/js/pages/OffersList/components/Modals/CurrencyModal.jsx
+++ src/js/pages/OffersList/components/Modals/CurrencyModal.jsx
@@ -1,3 +1,4 @@
+import React, {Component} from "react";
 import {Modal, ModalBody} from "reactstrap";
 import ClearButton from "../ClearButton";
 import {Highlighter, Typeahead} from "react-bootstrap-typeahead";
@@ -5,69 +6,81 @@ import {getOptionLabel} from "react-bootstrap-typeahead/lib/utils";
 import RoundedIcon from "../../../../ui/RoundedIcon";
 import Separator from "../../../MyProfile/components/Separator";
 import PropTypes from "prop-types";
-import React from "react";
 import {withTranslation} from "react-i18next";
 
-const CurrencyModal = ({t, onClose, selected, currencies, changeCurrency, offers}) => {
-  const defaultSelectedValue = [];
-  if (selected) {
-    const currency = currencies.find(x => x.id === selected);
-    defaultSelectedValue.push(currency);
+class CurrencyModal extends Component {
+  constructor(props) {
+    super(props);
+    this.currentValue = null;
+    this.lastKeyIsArrow = false;
   }
-  return (
-    <Modal isOpen={true} toggle={onClose} backdrop={true} className="filter-modal">
-      <ClearButton t={t} onClear={() => changeCurrency('')} close={onClose}/>
-      <ModalBody>
-        <Typeahead className="mb-3 mt-5"
-                   id="fiatSelector"
-                   onChange={(items) => {
-                     if (items.length) {
-                       const item = items[0];
-                       changeCurrency(item.id);
-                     }
-                     onClose();
-                   }}
-                   options={currencies}
-                   labelKey="id"
-                   placeholder={t("fiatSelectorForm.placeholder")}
-                   onInputChange={(text) => {
-                     const symbol = currencies.find(x => x.label === text);
-                     if (symbol) {
-                       changeCurrency(symbol.id);
-                     }
-                   }}
-                   submitFormOnEnter={true}
-                   emptyLabel={t("fiatSelectorForm.emptyLabel")}
-                   defaultSelected={defaultSelectedValue}
-                   renderMenuItemChildren={(option, _props, idx) => {
-                     const currency = getOptionLabel(option, _props.labelKey);
-                     let nbOffersForCurrency = 0;
 
-                     offers.forEach(offer => {
-                       if (offer.currency === currency) {
-                         nbOffersForCurrency++;
+  render() {
+    const {t, onClose, selected, currencies, changeCurrency, offers} = this.props;
+    const defaultSelectedValue = [];
+    if (selected) {
+      const currency = currencies.find(x => x.id === selected);
+      defaultSelectedValue.push(currency);
+    }
+    return (
+      <Modal isOpen={true} toggle={onClose} backdrop={true} className="filter-modal">
+        <ClearButton t={t} onClear={() => changeCurrency('')} close={onClose}/>
+        <ModalBody>
+          <Typeahead className="mb-3 mt-5"
+                     id="fiatSelector"
+                     onChange={(items) => {
+                       if (items.length) {
+                         const item = items[0];
+                         changeCurrency(item.id);
+                         onClose();
                        }
-                     });
+                     }}
+                     onKeyDown={e => {
+                       if (e.key === 'Enter' && this.currentValue === '' && !this.lastKeyIsArrow) {
+                         changeCurrency('');
+                         return onClose();
+                       }
+                       this.lastKeyIsArrow = e.key === 'ArrowDown' || e.key === 'ArrowUp';
+                     }}
+                     options={currencies}
+                     labelKey="id"
+                     placeholder={t("fiatSelectorForm.placeholder")}
+                     onInputChange={(text) => {
+                       this.currentValue = text;
+                     }}
+                     submitFormOnEnter={true}
+                     emptyLabel={t("fiatSelectorForm.emptyLabel")}
+                     defaultSelected={defaultSelectedValue}
+                     renderMenuItemChildren={(option, _props, idx) => {
+                       const currency = getOptionLabel(option, _props.labelKey);
+                       let nbOffersForCurrency = 0;
 
-                     return (<div className="mt-2">
-                       <RoundedIcon bgColor="blue" text={option.symbol} className="d-inline-block mr-3" size="md"/>
-                       <Highlighter search={_props.text}>
-                         {currency}
-                       </Highlighter>
-                       <span className="text-muted ml-2 d-inline-block mb-2">
+                       offers.forEach(offer => {
+                         if (offer.currency === currency) {
+                           nbOffersForCurrency++;
+                         }
+                       });
+
+                       return (<div className="mt-2">
+                         <RoundedIcon bgColor="blue" text={option.symbol} className="d-inline-block mr-3" size="md"/>
+                         <Highlighter search={_props.text}>
+                           {currency}
+                         </Highlighter>
+                         <span className="text-muted ml-2 d-inline-block mb-2">
                         {option.label}
                      </span>
-                       <span className="text-muted float-right mt-1">
+                         <span className="text-muted float-right mt-1">
                        ({nbOffersForCurrency})
                      </span>
-                       {idx !== currencies.length - 1 && <Separator/>}
-                     </div>);
-                   }}
-        />
-      </ModalBody>
-    </Modal>
-  );
-};
+                         {idx !== currencies.length - 1 && <Separator/>}
+                       </div>);
+                     }}
+          />
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
 
 CurrencyModal.propTypes = {
   t: PropTypes.func,
