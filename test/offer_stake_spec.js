a660fb31bc70a767c0b4ea3c9fa8dde482172ee6
Richard Ramos <info@richardramos.me>
2020-02-05 09:35:57 -0400

    fix: test stake (#690)

--- test/offer_stake_spec.js
+++ test/offer_stake_spec.js
@@ -123,28 +123,28 @@ contract("Escrow", function() {
     it("price for each offers should increase exponentially", async() => {
       let amountToStake;
 
+      // 1st to 4th offer
+      for(let i = 0; i < 4; i++){
+        amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
+        assert.strictEqual(amountToStake, "6189000000000000");
+        receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
+        offerIds.push(receipt.events.OfferAdded.returnValues.offerId);
+      }
 
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "6189000000000000");
+      assert.strictEqual(amountToStake, "9101470588235294");
 
       receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
       offerIds.push(receipt.events.OfferAdded.returnValues.offerId);
 
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-
-      assert.strictEqual(amountToStake, "24756000000000000");
-
-      receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
-      offerIds.push(receipt.events.OfferAdded.returnValues.offerId);
-
-      amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "55701000000000000");
+      assert.strictEqual(amountToStake, "13106117647058823");
 
       receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
       offerIds.push(receipt.events.OfferAdded.returnValues.offerId);
 
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "99024000000000000");
+      assert.strictEqual(amountToStake, "17838882352941176");
 
       receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
       offerIds.push(receipt.events.OfferAdded.returnValues.offerId);
@@ -156,39 +156,30 @@ contract("Escrow", function() {
       currOffer = offerIds.pop();
       await OfferStore.methods.removeOffer(currOffer).send();
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "99024000000000000");
+      assert.strictEqual(amountToStake, "17838882352941176");
 
       currOffer = offerIds.pop();
       await OfferStore.methods.removeOffer(currOffer).send();
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "55701000000000000");
+      assert.strictEqual(amountToStake, "13106117647058823");
 
       currOffer = offerIds.pop();
       await OfferStore.methods.removeOffer(currOffer).send();
       amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "24756000000000000");
+      assert.strictEqual(amountToStake, "9101470588235294");
 
-      currOffer = offerIds.pop();
-      await OfferStore.methods.removeOffer(currOffer).send();
-      amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "6189000000000000");
-
+      for(let i = 0; i < 3; i++){
+        currOffer = offerIds.pop();
+        await OfferStore.methods.removeOffer(currOffer).send();
+        amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
+        assert.strictEqual(amountToStake, "6189000000000000");
+      }
     });
-
-    it("price for each offers should keep increasing exponentially", async() => {
-      let amountToStake;
-
-      amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
-      ethOfferId = receipt.events.OfferAdded.returnValues.offerId;
-
-      amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "24756000000000000");
-    });
-
 
     it("deleting an offer should refund the stake", async() => {
       let contractBalance, userBalance1, userBalance2;
+
+      ethOfferId = offerIds.pop();
 
       userBalance1 = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
       contractBalance = await web3.eth.getBalance(OfferStore.options.address);
@@ -203,17 +194,31 @@ contract("Escrow", function() {
       assert(userBalance1.lt(userBalance2), "User balance did not increase after refund");
     });
 
+    it("price for each offers should keep increasing exponentially", async() => {
+      let amountToStake;
+
+      // 1st to 4th offer
+      for(let i = 0; i < 4; i++){
+        amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
+        assert.strictEqual(amountToStake, "6189000000000000");
+        receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
+      }
+
+      amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
+      assert.strictEqual(amountToStake, "9101470588235294");
+    });
+
     it("releasing an escrow should refund the stake and decrease next offer price", async() => {
-      let contractBalance, userBalance1, userBalance2;
+      let userBalance1, userBalance2;
 
       // Create Offer
       const amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
+      assert.strictEqual(amountToStake, "9101470588235294");
+
       receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
       ethOfferId = receipt.events.OfferAdded.returnValues.offerId;
 
       userBalance1 = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      assert.strictEqual(contractBalance, "6189000000000000");
 
       // Create Escrow
       receipt = await Escrow.methods.createEscrow(ethOfferId, tradeAmount, 140, accounts[1], CONTACT_DATA, "L", "U").send({from: accounts[1]});
@@ -226,9 +231,6 @@ contract("Escrow", function() {
       // Release Escrow
       receipt = await Escrow.methods.release(escrowId).send({from: accounts[0]});
 
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      assert.strictEqual(contractBalance, web3.utils.toWei("0", "ether"));
-
       userBalance2 = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
       assert(userBalance1.lt(userBalance2), "User balance did not increase after refund");
 
@@ -236,87 +238,20 @@ contract("Escrow", function() {
       assert.strictEqual(stakeDetails.amount, "0");
     });
 
-    it("price for next offer should decrease", async() => {
+    it("price for next offer should not increase", async() => {
       const amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "6189000000000000");
+      assert.strictEqual(amountToStake, "9101470588235294");
     });
-
-    it("losing a dispute should slash the stake and send it to the burn address", async() => {
-      let contractBalance, burnAddressBalance1, burnAddressBalance2;
-
-      // Create Offer
-      const amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
-      ethOfferId = receipt.events.OfferAdded.returnValues.offerId;
-
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      burnAddressBalance1 = await web3.eth.getBalance(BURN_ADDRESS);
-      assert.strictEqual(contractBalance, "6189000000000000");
-
-      // Create Escrow
-      hash = await UserStore.methods.getDataHash("U", CONTACT_DATA).call({from: accounts[1]});
-      signature = await web3.eth.sign(hash, accounts[1]);
-      nonce = await UserStore.methods.user_nonce(accounts[1]).call();
-      receipt = await Escrow.methods.createEscrow(ethOfferId, tradeAmount, 140, accounts[1], CONTACT_DATA, "L", "U", nonce, signature).send({from: accounts[1]});
-      created = receipt.events.Created;
-      escrowId = created.returnValues.escrowId;
-
-      // Fund Escrow
-      receipt = await Escrow.methods.fund(escrowId).send({from: accounts[0], value: tradeAmount + feeAmount});
-
-      // Pay Escrow
-      await Escrow.methods.pay(escrowId).send({from: accounts[1]});
-
-      // Open dispute
-      await Escrow.methods.openCase(escrowId, '1').send({from: accounts[1]});
-
-      // Seller loses dispute
-      receipt = await Escrow.methods.setArbitrationResult(escrowId, ARBITRATION_SOLVED_BUYER).send({from: arbitrator});
-
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      assert.strictEqual(contractBalance, web3.utils.toWei("0", "ether"));
-
-      burnAddressBalance2 = await web3.eth.getBalance(BURN_ADDRESS);
-      assert.strictEqual(burnAddressBalance2, web3.utils.toBN(burnAddressBalance1).add(web3.utils.toBN("6189000000000000")).toString());
-
-      const stakeDetails = await OfferStore.methods.stakes(ethOfferId).call();
-      assert.strictEqual(stakeDetails.amount, "0");
-    });
-
-    it("price for next offer should not change after losing the dispute", async() => {
-      const amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
-      assert.strictEqual(amountToStake, "24756000000000000");
-    });
-
-    it("losing a dispute twice shoud not fail", async() => {
-      // Create Escrow
-      receipt = await Escrow.methods.createEscrow(ethOfferId, tradeAmount, 140, accounts[1], CONTACT_DATA, "L", "U").send({from: accounts[1]});
-      created = receipt.events.Created;
-      escrowId = created.returnValues.escrowId;
-
-      // Fund Escrow
-      receipt = await Escrow.methods.fund(escrowId).send({from: accounts[0], value: tradeAmount + feeAmount});
-
-      // Pay Escrow
-      await Escrow.methods.pay(escrowId).send({from: accounts[1]});
-
-      // Open dispute
-      await Escrow.methods.openCase(escrowId, '1').send({from: accounts[1]});
-
-      // Seller loses dispute
-      receipt = await Escrow.methods.setArbitrationResult(escrowId, ARBITRATION_SOLVED_BUYER).send({from: arbitrator});
-    });
-
+    
     it("winning a dispute should not release the stake (only succesful trades do)", async() => {
-      let contractBalance;
+      let initialContractBalance, finalContractBalance;
 
       // Create Offer
       const amountToStake = await OfferStore.methods.getAmountToStake(accounts[0]).call();
       receipt = await OfferStore.methods.addOffer(TestUtils.zeroAddress, CONTACT_DATA, "London", "USD", "Iuri", [0], 0, 0, 1, arbitrator).send({from: accounts[0], value: amountToStake});
       ethOfferId = receipt.events.OfferAdded.returnValues.offerId;
 
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      assert.strictEqual(contractBalance, "24756000000000000");
+      initialContractBalance = await web3.eth.getBalance(OfferStore.options.address);
 
       // Create Escrow
       hash = await UserStore.methods.getDataHash("U", CONTACT_DATA).call({from: accounts[1]});
@@ -338,8 +273,8 @@ contract("Escrow", function() {
       // Seller wins dispute
       receipt = await Escrow.methods.setArbitrationResult(escrowId, ARBITRATION_SOLVED_SELLER).send({from: arbitrator});
 
-      contractBalance = await web3.eth.getBalance(OfferStore.options.address);
-      assert.strictEqual(contractBalance, "24756000000000000");
+      finalContractBalance = await web3.eth.getBalance(OfferStore.options.address);
+      assert.strictEqual(finalContractBalance, initialContractBalance);
     });
   });
 
