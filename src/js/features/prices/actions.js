e229975716b3272ca71ea73c9fd454be40f4cf9d
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-02-05 08:35:32 -0500

    fix: add button to load particular asset if it failed to load (#688)

--- src/js/features/prices/actions.js
+++ src/js/features/prices/actions.js
@@ -1,3 +1,5 @@
-import { FETCH_EXCHANGE_RATE } from './constants';
+import { FETCH_EXCHANGE_RATE, LOAD_PRICE_FOR_ASSET_AND_CURRENCY } from './constants';
 
 export const fetchExchangeRates = () => ({type: FETCH_EXCHANGE_RATE});
+
+export const loadPriceForAssetAndCurrency = (asset, currency) => ({type: LOAD_PRICE_FOR_ASSET_AND_CURRENCY, asset, currency});
