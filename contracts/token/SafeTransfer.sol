fbd2290730599629b1e8afeb3eab6f1e22550258
Richard Ramos <info@richardramos.me>
2020-04-02 10:08:46 -0400

    fix: Replace SafeTransfer for a more readable version by OpenZeppelin

--- contracts/token/SafeTransfer.sol
+++ contracts/token/SafeTransfer.sol
@@ -1,48 +1,74 @@
-pragma solidity ^0.5.7;
+pragma solidity >=0.5.0 <0.6.0;
 
-import "../token/ERC20Token.sol";
+import "./ERC20Token.sol";
 
-
+/**
+ * @title SafeERC20
+ * @dev Adapted from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol
+ * and https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol
+ */
 contract SafeTransfer {
-    function _safeTransfer(ERC20Token _token, address _to, uint256 _value) internal returns (bool result) {
-        _token.transfer(_to, _value);
-        assembly {
-        switch returndatasize()
-            case 0 {
-            result := not(0)
-            }
-            case 32 {
-            returndatacopy(0, 0, 32)
-            result := mload(0)
-            }
-            default {
-            revert(0, 0)
-            }
-        }
-        require(result, "Unsuccessful token transfer");
+    
+    function _safeTransfer(ERC20Token token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
 
-    function _safeTransferFrom(
-        ERC20Token _token,
-        address _from,
-        address _to,
-        uint256 _value
-    ) internal returns (bool result)
-    {
-        _token.transferFrom(_from, _to, _value);
-        assembly {
-        switch returndatasize()
-            case 0 {
-            result := not(0)
-            }
-            case 32 {
-            returndatacopy(0, 0, 32)
-            result := mload(0)
-            }
-            default {
-            revert(0, 0)
-            }
+    function _safeTransferFrom(ERC20Token token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+    
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(ERC20Token token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves.
+
+        // A Solidity high level call has three parts:
+        //  1. The target address is checked to verify it contains contract code
+        //  2. The call itself is made, and success asserted
+        //  3. The return value is decoded, which in turn checks the size of the returned data.
+        // solhint-disable-next-line max-line-length
+        require(_isContract(address(token)), "SafeTransfer: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeTransfer: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeTransfer: ERC20 operation did not succeed");
         }
-        require(result, "Unsuccessful token transfer");
+    }
+
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function _isContract(address account) internal view returns (bool) {
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != accountHash && codehash != 0x0);
     }
 }
